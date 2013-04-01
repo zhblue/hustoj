972f55edfcddacb20c5505c13282087f3406f776
zhang haobin <newsclan@gmail.com>
2013-04-01 19:29:15 +0800

    chinese

--- trunk/web/admin/contest_edit.php
+++ trunk/web/admin/contest_edit.php
@@ -106,30 +106,30 @@ if (isset($_POST['syear']))
 
 <form method=POST >
 <?php require_once("../include/set_post_key.php");?>
-<p align=center><font size=4 color=#333399>Edit a Contest</font></p>
+<p align=center><font size=4 color=#333399>编辑比赛或作业</font></p>
 <input type=hidden name='cid' value=<?php echo $cid?>>
-<p align=left>Title:<input class=input-xxlarge type=text name=title size=71 value='<?php echo $title?>'></p>
-<p align=left>Start Time:<br>&nbsp;&nbsp;&nbsp;
-Year:<input class=input-mini  type=text name=syear value=<?php echo substr($starttime,0,4)?> size=4 >
-Month:<input class=input-mini  type=text name=smonth value='<?php echo substr($starttime,5,2)?>' size=2 >
-Day:<input class=input-mini  type=text name=sday size=2 value='<?php echo substr($starttime,8,2)?>'>
-Hour:<input class=input-mini  type=text name=shour size=2 value='<?php echo substr($starttime,11,2)?>'>
-Minute:<input class=input-mini  type=text name=sminute size=2 value=<?php echo substr($starttime,14,2)?>></p>
-<p align=left>End Time:<br>&nbsp;&nbsp;&nbsp;
+<p align=left>标题<input class=input-xxlarge type=text name=title size=71 value='<?php echo $title?>'></p>
+<p align=left>开始时间:<br>&nbsp;&nbsp;&nbsp;
+年<input class=input-mini  type=text name=syear value=<?php echo substr($starttime,0,4)?> size=4 >
+月<input class=input-mini  type=text name=smonth value='<?php echo substr($starttime,5,2)?>' size=2 >
+日<input class=input-mini  type=text name=sday size=2 value='<?php echo substr($starttime,8,2)?>'>
+时<input class=input-mini  type=text name=shour size=2 value='<?php echo substr($starttime,11,2)?>'>
+分<input class=input-mini  type=text name=sminute size=2 value=<?php echo substr($starttime,14,2)?>></p>
+<p align=left>结束时间:<br>&nbsp;&nbsp;&nbsp;
 
-Year:<input class=input-mini  type=text name=eyear value=<?php echo substr($endtime,0,4)?> size=4 >
-Month:<input class=input-mini  type=text name=emonth value=<?php echo substr($endtime,5,2)?> size=2 >
-Day:<input class=input-mini  type=text name=eday size=2 value=<?php echo substr($endtime,8,2)?>>
-Hour:<input class=input-mini  type=text name=ehour size=2 value=<?php echo substr($endtime,11,2)?>> 
-Minute:<input class=input-mini  type=text name=eminute size=2 value=<?php echo substr($endtime,14,2)?>></p>
+年<input class=input-mini  type=text name=eyear value=<?php echo substr($endtime,0,4)?> size=4 >
+月<input class=input-mini  type=text name=emonth value=<?php echo substr($endtime,5,2)?> size=2 >
+日<input class=input-mini  type=text name=eday size=2 value=<?php echo substr($endtime,8,2)?>>
+时<input class=input-mini  type=text name=ehour size=2 value=<?php echo substr($endtime,11,2)?>> 
+分<input class=input-mini  type=text name=eminute size=2 value=<?php echo substr($endtime,14,2)?>></p>
 
-Public/Private:<select name=private>
-	<option value=0 <?php echo $private=='0'?'selected=selected':''?>>Public</option>
-	<option value=1 <?php echo $private=='1'?'selected=selected':''?>>Private</option>
+作业/考试<select name=private>
+	<option value=0 <?php echo $private=='0'?'selected=selected':''?>>作业</option>
+	<option value=1 <?php echo $private=='1'?'selected=selected':''?>>考试</option>
 </select>
-<br>Problems:<input class=input-xxlarge type=text size=60 name=cproblem value='<?php echo $plist?>'>
+<br>题目<input class=input-xxlarge type=text size=60 name=cproblem value='<?php echo $plist?>'>
 
- Language:<select name="lang[]"  multiple="multiple"    style="height:220px">
+ 可选编程语言<select name="lang[]"  multiple="multiple"    style="height:220px">
 <?php
 $lang_count=count($language_ext);
 
@@ -150,7 +150,7 @@ if(isset($_COOKIE['lastlang'])) $lastlang=$_COOKIE['lastlang'];
 	
 
 <br>
-<p align=left>Description:<br><!--<textarea rows=13 name=description cols=80></textarea>-->
+<p align=left>作业或考试说明<br><!--<textarea rows=13 name=description cols=80></textarea>-->
 
 <?php
 $fck_description = new FCKeditor('description') ;
