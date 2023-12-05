77f5b73187e26b1aa003a96bc0d48cf4e7610120
张浩斌 <newsclan@gmail.com>
2023-12-06 07:21:26 +0800

    Update init.php

--- trunk/web/include/init.php
+++ trunk/web/include/init.php
@@ -126,6 +126,7 @@ if(isset($OJ_BG)&&$OJ_BG=="bing"){
            require_once(dirname(__FILE__)."/curl.php");
            $data=curl_get("https://cn.bing.com");
            $OJ_BG=getPartByMark($data,"<link rel=\"preload\" href=\"","\" as=\"image\" id=\"preloadBg\"");
+	   $OJ_BG="https://cn.bing.com/".$OJ_BG;
            if($OJ_BG)file_put_contents($bg_file,$OJ_BG);
 	   else touch($bg_file);
    }else{
