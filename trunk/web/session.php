6ab1a59e73ca8fbeb5b05668859ce435499f04d0
张浩斌 <newsclan@gmail.com>
2023-12-31 17:51:05 +0800

    Update session.php

--- trunk/web/session.php
+++ trunk/web/session.php
@@ -1,4 +1,18 @@
 <?php
-   session_start();
+require_once(dirname(__FILE__)."/include/db_info.inc.php");
+if(isset($_SESSION[$OJ_NAME."_user_id"])){
+        $user_id=$_SESSION[$OJ_NAME."_user_id"];
+        $defunct=pdo_query("select defunct from users where user_id=?",$user_id);
+        if(is_array($defunct)&&$defunct[0][0]=="Y"){
+                unset($_SESSION[$OJ_NAME.'_'.'user_id']);
+                setcookie($OJ_NAME."_user","");
+                setcookie($OJ_NAME."_check","");
+                session_destroy();
 ?>
+                <script>window.top.location.href="index.php";</script>
+<?php
+        }
+}
+?>
+
    <script>window.setTimeout('window.location.reload();',300000);</script>
