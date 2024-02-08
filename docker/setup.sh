6c1b3a46bb9d1ed0f1b56cc7ff8333ae5a2221b1
张浩斌 <newsclan@gmail.com>
2024-02-08 13:39:14 +0800

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -119,7 +119,9 @@ apt install build-essential dh-make devscripts dpkg-dev debhelper quilt lintian 
 wget -O deb.zip https://github.com/zhblue/hustoj-deb-ubuntu/archive/refs/heads/master.zip
 unzip deb.zip
 mv hustoj-deb-ubuntu-master/* ./
+
 VERSION=`date +"%y.%m.%d"`
+sed -i "s/0.0.1/$VERSION/g" debian/changelog
 
 dpkg-buildpackage
 cd ..
