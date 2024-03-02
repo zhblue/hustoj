47aa96013b557a9d6dbe343648a43dba59ef97e9
张浩斌 <newsclan@gmail.com>
2024-03-02 16:53:01 +0800

    Update index.php

--- trunk/web/template/syzoj/index.php
+++ trunk/web/template/syzoj/index.php
@@ -33,7 +33,7 @@
 <?php
 /* 本月之星  */
 $month_id=mysql_query_cache("select solution_id from solution where  in_date<date_add(curdate(),interval -day(curdate())+1 DAY) order by solution_id desc limit 1;");
-if(is_array( $month_id)) $month_id=$month_id[0][0];else $month_id=0;
+if(is_array( $month_id) && isset($month_id[0][0]) ) $month_id=$month_id[0][0];else $month_id=0;
 if($NOIP_flag[0]==0)$view_month_rank=mysql_query_cache("select user_id,nick,count(distinct(problem_id)) ac from solution where solution_id>$month_id and problem_id>0 and result=4 group by user_id,nick order by ac desc limit 10");
             if ( is_array($view_month_rank) ) {
         ?>
