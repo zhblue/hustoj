4618b72aadc5f08bcbd609fd7fc0572588675a30
张浩斌 <newsclan@gmail.com>
2024-03-06 08:52:58 +0800

    Update problem_list.php

--- trunk/web/admin/problem_list.php
+++ trunk/web/admin/problem_list.php
@@ -117,7 +117,8 @@ echo "</select>";
             echo "<td><a href=problem_df_change.php?id=".$row['problem_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey'].">".($row['defunct']=="N"?"<span titlc='click to reserve it' class=green>$MSG_AVAILABLE</span>":"<span class=red title='click to be available'>$MSG_RESERVED</span>")."</a><td>";
             if($OJ_SAE||function_exists("system")){
     ?>
-              <a href=# onclick='javascript:if(confirm("Delete?")) location.href="problem_del.php?id=<?php echo $row['problem_id']?>&getkey=<?php echo $_SESSION[$OJ_NAME.'_'.'getkey']?>"'>
+             <a href=# onclick='javascript:if(confirm("<?php echo $MSG_DELETE."[".htmlentities($row['title'],ENT_QUOTES,"UTF-8")?>] ?    ")) 
+		     location.href="problem_del.php?id=<?php echo $row['problem_id']?>&getkey=<?php echo $_SESSION[$OJ_NAME.'_'.'getkey']?>"'>
 <?php echo $MSG_DELETE ?>
               </a>
         <?php
