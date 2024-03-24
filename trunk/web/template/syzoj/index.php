ba2409f7aaf5d2098cc0b09bf43bd8589ed67ab3
张浩斌 <newsclan@gmail.com>
2024-03-24 15:09:28 +0800

    Update index.php (#1000)

--- trunk/web/template/syzoj/index.php
+++ trunk/web/template/syzoj/index.php
@@ -34,7 +34,7 @@
 /* 本月之星  */
 $month_id=mysql_query_cache("select solution_id from solution where  in_date<date_add(curdate(),interval -day(curdate())+1 DAY) order by solution_id desc limit 1;");
 if(is_array( $month_id) && isset($month_id[0][0]) ) $month_id=$month_id[0][0];else $month_id=0;
-if($NOIP_flag[0]==0)$view_month_rank=mysql_query_cache("select user_id,nick,count(distinct(problem_id)) ac from solution where solution_id>$month_id and problem_id>0 and result=4 group by user_id,nick order by ac desc limit 10");
+if($NOIP_flag[0]==0)$view_month_rank=mysql_query_cache("select user_id,nick,count(distinct(problem_id)) ac from solution where solution_id>$month_id and problem_id>0  and user_id not in (".$OJ_RANK_HIDDEN.")  and result=4 group by user_id,nick order by ac desc limit 10");
             if ( is_array($view_month_rank) ) {
         ?>
             <h4 class="ui top attached block header"><i class="ui star icon"></i><?php echo "本月之星"?></h4>
