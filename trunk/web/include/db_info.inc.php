8e5ac819cedd96d6cd9eceb75d7132c417513dcc
张浩斌 <newsclan@gmail.com>
2024-05-28 08:33:18 +0800

    Update db_info.inc.php

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -48,6 +48,7 @@ static  $SAE_STORAGE_ROOT="http://hustoj-web.stor.sinaapp.com/";  //新浪云存
 static  $OJ_CDN_URL="";  // 如果服务器带宽较小，可选用他人同版本的OJ作为静态资源来源 http://cdn.hustoj.com/ 
 static  $OJ_TEMPLATE="syzoj"; //使用的默认模板,template目录下的每个子目录都是一个模板, [bs3 mdui sweet syzoj mario bshark] work with discuss3
 static 	$OJ_BG="/image/background.jpg";  //双引号里面填写背景图片的url，默认bing，每小时自动更换bing.com最新背景。
+// $OJ_BG="/image/bing".date('H').".jpg";  //每个整点更换壁纸，例如准备bing[00~23].jpg在image目录。
 static  $OJ_LOGIN_MOD="hustoj"; //需要在include目录下配置login-xxxx.php来调用其他登录模块。
 static  $OJ_REGISTER=true; //允许注册新用户
 static  $OJ_REG_NEED_CONFIRM=false; //新注册用户需要审核
