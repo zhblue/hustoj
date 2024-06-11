24ce327ef85cc8e46d028170062d542b92b0a44b
张浩斌 <newsclan@gmail.com>
2024-06-11 08:22:04 +0800

    Update th.php

--- trunk/web/lang/th.php
+++ trunk/web/lang/th.php
@@ -208,7 +208,10 @@
   $MSG_Ended="Finished";
   $MSG_Login="Please Login";
   $MSG_JUDGER="Judger";
-  
+  $MSG_DOWNLOAD="Download";
+  $MSG_SHOW="Show";
+  $MSG_HIDE="Hide";
+
   $MSG_SOURCE_NOT_ALLOWED_FOR_EXAM="You can't browse early code during examing .";
   $MSG_BBS_NOT_ALLOWED_FOR_EXAM="You can't use bbs during examing.";
   $MSG_MODIFY_NOT_ALLOWED_FOR_EXAM="You can't change password during examing or contest on site.";
