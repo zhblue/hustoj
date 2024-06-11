556a7eaf5a3cafe69c2e3ab5f053d251eee02ebb
张浩斌 <newsclan@gmail.com>
2024-06-11 14:06:54 +0800

    Update viewnews.php

--- trunk/web/template/syzoj/viewnews.php
+++ trunk/web/template/syzoj/viewnews.php
@@ -1,4 +1,4 @@
-<?php $show_title="公告 - $OJ_NAME"; ?>
+<?php $show_title="$MSG_NEWS - $OJ_NAME"; ?>
 <?php include("template/$OJ_TEMPLATE/header.php");?>
 <div class="padding">
     <h1><?php echo $news_title ?></h1>
