c0f127a06eaf225ef7722cd501c1f17c34e27a38
张浩斌 <newsclan@gmail.com>
2024-06-11 08:44:08 +0800

    Update faqs.news.php

--- trunk/web/template/syzoj/faqs.news.php
+++ trunk/web/template/syzoj/faqs.news.php
@@ -1,7 +1,7 @@
 <?php $show_title="$MSG_FAQ - $OJ_NAME"; ?>
 <?php include("template/$OJ_TEMPLATE/header.php");?>
 <div class="padding">
-    <h1 class="ui center aligned header">帮助</h1>
+    <h1 class="ui center aligned header"><?php echo $MSG_FAQ ?></h1>
     <div style="font-content">
              <?php echo $view_faqs?>
     </div>
