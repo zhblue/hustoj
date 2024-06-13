84275047193326b7cb782b2134ceebe5060f8784
张浩斌 <newsclan@gmail.com>
2024-06-13 10:49:31 +0800

    Update modifypage.php

--- trunk/web/template/syzoj/modifypage.php
+++ trunk/web/template/syzoj/modifypage.php
@@ -51,7 +51,7 @@
   <div class="center"> <a name='MyOJ'>&nbsp;</a> <label >My OJ:</label>
           <form action="saasinit.php" method="post" role="form" class="ui form">
                 <div class="field">
-                    <label for="template">模板</label>
+                    <label for="template"><?php echo $MSG_TEMPLATE ?></label>
                     <select name="template" class="form-control" >
                                 <option>bs3</option>
                                 <option>mdui</option>
@@ -63,18 +63,18 @@
                 </div>
 
                 <div class="field">
-                    <label for="friendly">友善级别</label>
+                    <label for="friendly"><?php echo $MSG_FRIENDLY_LEVEL ?></label>
                     <select name="friendly" class="form-control" >
-                                <option value=0>0=不友善</option>
-                                <option value=1>1=0+中国时区</option>
-                                <option value=2>2=1+强制中文</option>
-                                <option value=3>3=2+显示对比,关闭验证码</option>
-                                <option value=4>4=3+开启内邮,代码自动分享</option>
-                                <option value=5>5=4+开启测试运行</option>
-                                <option value=6>6=5+保持登陆状态</option>
-                                <option value=7>7=6+开启讨论版</option>
-                                <option value=8>8=7+可以下载测试数据</option>
-                                <option value=9>9=8+允许访客提交</option>
+                                <option value=0>0=<?php echo   $MSG_FRIENDLY_L0 ?></option>
+                                <option value=1>1=0+<?php echo   $MSG_FRIENDLY_L1 ?></option>
+                                <option value=2>2=1+<?php echo   $MSG_FRIENDLY_L2 ?></option>
+                                <option value=3>3=2+<?php echo   $MSG_FRIENDLY_L3 ?></option>
+                                <option value=4>4=3+<?php echo   $MSG_FRIENDLY_L4 ?></option>
+                                <option value=5>5=4+<?php echo   $MSG_FRIENDLY_L5 ?></option>
+                                <option value=6>6=5+<?php echo   $MSG_FRIENDLY_L6 ?></option>
+                                <option value=7>7=6+<?php echo   $MSG_FRIENDLY_L7 ?></option>
+                                <option value=8>8=7+<?php echo   $MSG_FRIENDLY_L8 ?></option>
+                                <option value=9>9=8+<?php echo   $MSG_FRIENDLY_L9 ?></option>
                     </select>
                 </div>
                 <button name="submit" type="submit" class="ui button">重新初始化</button>
