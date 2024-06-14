c321f6c8529539d9f883b02c044f095667cb1d93
张浩斌 <newsclan@gmail.com>
2024-06-14 10:25:01 +0800

    Update problem_add_page.php

--- trunk/web/admin/problem_add_page.php
+++ trunk/web/admin/problem_add_page.php
@@ -180,7 +180,7 @@
 	preview.find("#hint .md").each(function(){
 		$(this).html(marked.parse($(this).html()));
 	});
-	$("#previewFrame")[0].contentWindow.MathJax.typeset();
+	if($("#previewFrame")[0] != undefined) $("#previewFrame")[0].contentWindow.MathJax.typeset();
   }
  
    $(document).ready(function(){
