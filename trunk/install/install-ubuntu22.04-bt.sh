09bcd68b7401fac852934e2b684e1f2109f95ba1
Tiang-88 <51110833+Tiang-88@users.noreply.github.com>
2024-06-25 06:58:18 +0800

    如果还是podman会导致判不了题 (#1026)

--- trunk/install/install-ubuntu22.04-bt.sh
+++ trunk/install/install-ubuntu22.04-bt.sh
@@ -140,7 +140,7 @@ else
          sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
          sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/g" /home/judge/etc/judge.conf
          sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
-	sed -i "s|OJ_DOCKER_PATH=/usr/bin/docker|OJ_DOCKER_PATH=/usr/bin/podman|g" /home/judge/etc/judge.conf
+	sed -i "s|OJ_DOCKER_PATH=/usr/bin/docker|OJ_DOCKER_PATH=/usr/bin/docker|g" /home/judge/etc/judge.conf
 fi
 clear
 reset
