0b938661903dfab36cbae5a3b255c062a7055313
张浩斌 <newsclan@gmail.com>
2024-07-07 09:24:25 +0800

    Update ceinfo.php

--- trunk/web/ceinfo.php
+++ trunk/web/ceinfo.php
@@ -1,8 +1,6 @@
 <?php
- 
-
-	$cache_time=10;
-	$OJ_CACHE_SHARE=false;
+$cache_time=10;
+$OJ_CACHE_SHARE=false;
 	require_once('./include/cache_start.php');
         require_once('./include/db_info.inc.php');
 	require_once('./include/setlang.php');
