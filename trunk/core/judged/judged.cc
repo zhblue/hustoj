12559ef7662af785e9031b54bf19bc3f3bb0018d
张浩斌 <newsclan@gmail.com>
2024-07-11 09:19:31 +0800

    Update judged.cc

--- trunk/core/judged/judged.cc
+++ trunk/core/judged/judged.cc
@@ -659,7 +659,7 @@ int work() {
 
 	}
 	int NOHANG=0;
-	if(( max_running > 7 || oj_dedicated ) && (rand()%100>20) ) NOHANG=WNOHANG;    // CPU 占用大约80%左右，不要打满
+	if(( max_running >3 || oj_dedicated ) && ( max_running > 7 || rand()%100>20 ) ) NOHANG=WNOHANG;    // CPU小于4个 占用大约80%左右，不要打满；大于7个，瓶颈转为IO，全力工作
 	while ((tmp_pid = waitpid(-1, NULL, NOHANG )) > 0) {       // if run dedicated judge using WNOHANG
 		for (i = 0; i < max_running; i++){     // get the client id
 			if (ID[i] == tmp_pid){
