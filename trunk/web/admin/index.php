5062b8c511e3a6147ddbb885f211dc28c3c31ae6
张浩斌 <newsclan@gmail.com>
2024-07-12 17:11:16 +0800

    Update index.php

--- trunk/web/admin/index.php
+++ trunk/web/admin/index.php
@@ -1,8 +1,10 @@
-<html>
+<?php
+        require_once("../include/db_info.inc.php");
+?><html>
 <head>
-  <title>JudgeOnline Administration</title>
+<title><?php echo $OJ_NAME.$MSG_ADMIN?></title>
 </head>
-<frameset cols="20%,*">
+<frameset cols="12%,*">
   <frame name="menu" src="menu2.php">
   <frame name="main" src="help.php">
   <noframes>
