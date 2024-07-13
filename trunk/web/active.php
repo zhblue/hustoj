3f81519c7bf6b9bc42f526bf35d7fc087908d778
张浩斌 <newsclan@gmail.com>
2024-07-13 15:43:05 +0800

    Update active.php

--- trunk/web/active.php
+++ trunk/web/active.php
@@ -4,8 +4,10 @@
 	require_once("./include/const.inc.php");
 	require_once("./include/my_func.inc.php");
 	$code=$_GET['code'];
-  	$sql="update `users` set defunct='N',activecode=''  WHERE `activecode`=? and defunct='Y' ";
-        $result=pdo_query($sql,$code);
-	var_dump( $result);
+	if($code!=""){
+	  	$sql="update `users` set defunct='N',activecode=''  WHERE `activecode`=? and `activecode`!='' and defunct='Y' ";
+	        $result=pdo_query($sql,$code);
+	}
+	//var_dump( $result);
 	header("location:loginpage.php");
 ?>
