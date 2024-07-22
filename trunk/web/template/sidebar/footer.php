b407620ee8c5f2d568f40eb6fb31027d0cb65982
张浩斌 <newsclan@gmail.com>
2024-07-22 11:28:10 +0800

    move footer

--- trunk/web/template/sidebar/footer.php
+++ trunk/web/template/sidebar/footer.php
@@ -22,21 +22,6 @@
     }
     </style>
     <?php include(dirname(__FILE__)."/js.php");?>
-    <div class="footer">
-        <div class="ui center aligned container" title="如果你想移除这个信息，请编辑template/syzoj/footer.php" >
-            <div><?php echo $domain==$DOMAIN?$OJ_NAME:ucwords($OJ_NAME)."'s OJ"?> is powered by <a style="color: inherit !important;" class=" " title="GitHub"
-                    target="_blank" rel="noreferrer noopener" href="https://github.com/zhblue/hustoj">HUSTOJ</a>, Theme
-                by <a style="color: inherit !important;" href="https://github.com/syzoj">SYZOJ</a></div>
-         <!--   <div> Running on <a href='https://debian.org' target='_blank'>Debian11</a> / <a href='https://www.loongson.cn' target='_blank'>Loongson 3A3000</a> </div> -->
-            <?php if ($OJ_BEIAN) { ?>
-            <div>
-            <img src="image/icp.png">
-                <a href="https://beian.miit.gov.cn/" style="text-decoration: none; color: #444444;"
-                    target="_blank"><?php echo $OJ_BEIAN; ?></a>
-            </div>
-            <?php } ?>
-        </div>
-    </div>
     </div>
 
 </footer>
