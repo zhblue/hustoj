648f05fab5a9cec8473490a10ab5b24837540488
张浩斌 <newsclan@gmail.com>
2024-08-05 09:11:25 +0800

    Update problem_add_page.php

--- trunk/web/admin/problem_add_page.php
+++ trunk/web/admin/problem_add_page.php
@@ -43,8 +43,8 @@
         </p>
         <p align=left>
           <?php echo "<h4>".$MSG_Description."(<64kB)</h4>"?>
-	  <textarea class="kindeditor" rows=13 name=description cols=80><span class='md'>
-</span></textarea><br>
+	  <textarea class="kindeditor" rows=13 name=description cols=80><span class='md auto_select'>&nbsp;
+&nbsp;</span></textarea><br>
         </p>
         <p align=left>
           <?php echo "<h4>".$MSG_Input."(<64kB)</h4>"?>
