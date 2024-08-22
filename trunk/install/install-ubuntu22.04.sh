9b596d9351a163dc48534a3b8c693c31a4b0a02e
张浩斌 <newsclan@gmail.com>
2024-08-23 07:53:23 +0800

    Update install-ubuntu22.04.sh

--- trunk/install/install-ubuntu22.04.sh
+++ trunk/install/install-ubuntu22.04.sh
@@ -146,6 +146,8 @@ if grep "added by hustoj" /etc/nginx/sites-enabled/default ; then
         echo "default site modified!"
 else
         echo "modify the default site"
+        
+        sed -i "s#listen 80 default_server;#listen 80 default_server backlog=4096;#g" /etc/nginx/sites-enabled/default
         sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
         sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
         sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
