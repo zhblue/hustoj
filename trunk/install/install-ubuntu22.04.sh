d93eabf1faea0ffa5b2f6ce0e15786622a129a24
张浩斌 <newsclan@gmail.com>
2024-08-27 07:36:29 +0800

    Update install-ubuntu22.04.sh

--- trunk/install/install-ubuntu22.04.sh
+++ trunk/install/install-ubuntu22.04.sh
@@ -40,7 +40,7 @@ add-apt-repository -y restricted
 apt-get update && apt-get -y upgrade
 
 #apt-get install -y subversion
-/usr/sbin/useradd -m -u 1536 judge
+/usr/sbin/useradd -m -u 1536 -s /sbin/nologin judge
 
 cd /home/judge/ || exit
 
