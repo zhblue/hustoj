2bcf9da0c58f49bb2efdd6c600442c1c3636db33
张浩斌 <newsclan@gmail.com>
2024-08-26 08:42:09 +0800

    Update oomsaver.sh

--- trunk/install/oomsaver.sh
+++ trunk/install/oomsaver.sh
@@ -5,3 +5,4 @@ pgrep -f "php-fpm" | while read PID;do echo -17 > /proc/$PID/oom_score_adj;done
 pgrep -f "mysql" | while read PID;do echo -17 > /proc/$PID/oom_score_adj;done
 if test -e /swap ;then swapon /swap ;fi
 echo 100 > /proc/sys/vm/swappiness
+echo "set global connection_memory_limit=20971520;" | mysql
