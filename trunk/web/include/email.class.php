ff3ebeb9abe5c400524495740c49a4d40a51358c
张浩斌 <newsclan@gmail.com>
2024-09-06 13:16:00 +0800

    Update email.class.php

--- trunk/web/include/email.class.php
+++ trunk/web/include/email.class.php
@@ -8,14 +8,9 @@ require dirname(__FILE__).'/PHPMailer.php';
 require dirname(__FILE__).'/SMTP.php';
 
 function email($address,$mailtitle,$mailcontent,$html=""){
-        //******************** 配置信息 ********************************
+        
         global $OJ_NAME,$SMTP_SERVER,$SMTP_PORT,$SMTP_USER,$SMTP_PASS;
-        $SMTP_SERVER = "smtp.qq.com";           //SMTP服务器，通常在邮箱的smtp/pop3设置中可以查询到，推荐用企业邮箱发信，避免被识别为垃圾邮件
-        $SMTP_PORT =587;                           //SMTP服务器端口，通常是25，有的服务器支持80（阿里云）、465(网易)、587（QQ）以适应不同的网络防火墙配置
-        $SMTP_USER = "mailer@qq.com";      //SMTP服务器的用户名（通常就是发件人的邮箱地址）, 这里修改后视为邮件配置生效，若配置不当可能导致部分页面超时。
-        $SMTP_PASS = "your_smpt_auth_password";       //由邮箱系统生成的口令 (SMTP服务器的密码)
-        //通常只需修改上面的4个设置。
-        //************************ 配置信息 ****************************
+
 	
 	$mail = new PHPMailer(true);
         //未经配置的系统，跳过发信步骤。
