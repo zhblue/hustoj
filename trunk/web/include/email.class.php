fb7139668290e4a7bffe2f929539957a896c5f2f
张浩斌 <newsclan@gmail.com>
2024-09-06 13:15:22 +0800

    Update email.class.php

--- trunk/web/include/email.class.php
+++ trunk/web/include/email.class.php
@@ -9,33 +9,30 @@ require dirname(__FILE__).'/SMTP.php';
 
 function email($address,$mailtitle,$mailcontent,$html=""){
         //******************** 配置信息 ********************************
-        global $OJ_NAME;
-        $smtpserver = "smtp.qq.com";           //SMTP服务器，通常在邮箱的smtp/pop3设置中可以查询到，推荐用企业邮箱发信，避免被识别为垃圾邮件
-        $smtpserverport =587;                           //SMTP服务器端口，通常是25，有的服务器支持80（阿里云）、465(网易)、587（QQ）以适应不同的网络防火墙配置
-        $smtpusermail = "mailer@qq.com";      //SMTP服务器的用户名（通常就是发件人的邮箱地址）, 这里修改后视为邮件配置生效，若配置不当可能导致部分页面超时。
-        $smtppass = "your_smpt_auth_password";       //由邮箱系统生成的口令 (SMTP服务器的密码)
+        global $OJ_NAME,$SMTP_SERVER,$SMTP_PORT,$SMTP_USER,$SMTP_PASS;
+        $SMTP_SERVER = "smtp.qq.com";           //SMTP服务器，通常在邮箱的smtp/pop3设置中可以查询到，推荐用企业邮箱发信，避免被识别为垃圾邮件
+        $SMTP_PORT =587;                           //SMTP服务器端口，通常是25，有的服务器支持80（阿里云）、465(网易)、587（QQ）以适应不同的网络防火墙配置
+        $SMTP_USER = "mailer@qq.com";      //SMTP服务器的用户名（通常就是发件人的邮箱地址）, 这里修改后视为邮件配置生效，若配置不当可能导致部分页面超时。
+        $SMTP_PASS = "your_smpt_auth_password";       //由邮箱系统生成的口令 (SMTP服务器的密码)
         //通常只需修改上面的4个设置。
-        $smtpuser = "$smtpusermail";           //SMTP服务器的用户帐号,通常就是邮箱，个别情况服务器支持一个账号多个邮箱地址的可能不同。
-        $smtpemailto =$address;               //发送给谁
-        $mailtype = "TXT";//邮件格式（HTML/TXT）,TXT为文本邮件
         //************************ 配置信息 ****************************
 	
 	$mail = new PHPMailer(true);
         //未经配置的系统，跳过发信步骤。
-	if( $smtpusermail != "mailer@qq.com") {      // 不要修改这个检测标记
+	if( $SMTP_USER != "mailer@qq.com") {      // 不要修改这个检测标记
 		try {
 		    //Server settings
 		    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 		    $mail->isSMTP();                                            //Send using SMTP
-		    $mail->Host       = $smtpserver;                     //Set the SMTP server to send through
+		    $mail->Host       = $SMTP_SERVER;                     //Set the SMTP server to send through
 		    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		    $mail->Username   = $smtpuser;                     //SMTP username
-		    $mail->Password   = $smtppass;                               //SMTP password
+		    $mail->Username   = $SMTP_USER;                     //SMTP username
+		    $mail->Password   = $SMTP_PASS;                               //SMTP password
 		    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-		    $mail->Port       = $smtpserverport;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+		    $mail->Port       = $SMTP_PORT;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 		    //Recipients
-		    $mail->setFrom($smtpuser, $OJ_NAME );
+		    $mail->setFrom($SMTP_USER, $OJ_NAME );
 		    $mail->addAddress($address, $OJ_NAME.' User');     //Add a recipient
 		   // $mail->addAddress('ellen@example.com');               //Name is optional
 		   // $mail->addReplyTo('info@example.com', 'Information');
