58e0543578f474a3635740c64426deabe57e4f8f
张浩斌 <newsclan@gmail.com>
2024-09-07 11:31:07 +0800

    Update registerpage.php

--- trunk/web/template/syzoj/registerpage.php
+++ trunk/web/template/syzoj/registerpage.php
@@ -5,37 +5,37 @@
   <div class="ui error message" id="error" data-am-alert hidden>
     <p id="error_info"></p>
   </div>
-          <form action="register.php" method="post" role="form" class="ui form">
-                <div class="field">
+         <form action="register.php" method="post" role="form" class="ui form">
+                <div class="font-content">
                     <label for="username"><?php echo $MSG_USER_ID?>*</label>
                     <input name="user_id" class="form-control" placeholder="" type="text">
                 </div>
-                <div class="field">
+                <div class="font-content">
                     <label for="username"><?php echo $MSG_NICK?>*</label>
                     <input name="nick" placeholder="" type="text">
                 </div>
                 <div class="two fields">
-                    <div class="field">
+                    <div class="font-content">
                     <label class="ui header"><?php echo $MSG_PASSWORD?>*</label>
                       <input name="password" placeholder="" type="password">
                     </div>
-                    <div class="field">
+                    <div class="font-content">
                       <label class="ui header"><?php echo $MSG_REPEAT_PASSWORD?>*</label>
                       <input name="rptpassword" placeholder="" type="password">
                     </div>
                 </div>
-                <div class="field">
+                <div class="font-content">
                     <label for="username"><?php echo $MSG_SCHOOL?></label>
                     <input name="school" placeholder="" type="text" value="">
                 </div>
-                <div class="field">
+                <div class="font-content">
                     <label for="email"><?php echo $MSG_EMAIL?>*</label>
                     <input name="email" placeholder="" type="text">
                 </div>
                 <?php if($OJ_VCODE){?>
-                  <div class="field">
+                  <div class="font-content">
                     <label for="email"><?php echo $MSG_VCODE?>*</label>
-                    <input name="vcode" class="form-control" placeholder="" type="text" autocomplete=off >
+                    <input name="vcode" class="form-control" placeholder="" type="text">
                     <img alt="click to change" src="vcode.php" onclick="this.src='vcode.php?'+Math.random()" height="30px">
                   </div>
                 <?php }?>
