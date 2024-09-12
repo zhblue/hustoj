ea919d9bc4e6da8b0bf963eb2f652a57aff7877a
张浩斌 <newsclan@gmail.com>
2024-09-12 10:22:04 +0800

    Update fa.php

--- trunk/web/lang/fa.php
+++ trunk/web/lang/fa.php
@@ -321,6 +321,10 @@
 	$MSG_STRING_INDEX_OUT_OF_BOUNDS_EXCEPTION="The character subscript of the string is out of range, check the parameters of subString, charAt and other methods.";
         $MSG_WRONG_OUTPUT_TYPE_EXCEPTION="Are you using an number as a char? Did you print out any Non-printable characters?";
         $MSG_NON_ZERO_RETURN="Do NOT return non-zero value in your main() function , or system will regard it as an error. ";
+  $MSG_EXPECTED="Expected Output";
+  $MSG_YOURS="Your Output";
+  $MSG_FILENAME="Filename";
+  $MSG_SIZE="Size";
 
 	// template/../ceinfo.php
 	$MSG_ERROR_EXPLAIN="Expain";
