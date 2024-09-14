358ba2e55b68ead7c697574f1a1811adcaee5b2c
张浩斌 <newsclan@gmail.com>
2024-09-15 07:39:14 +0800

    use name

--- trunk/web/template/sidebar/conteststatus.php
+++ trunk/web/template/sidebar/conteststatus.php
@@ -156,6 +156,6 @@
         var oj_mark= <?php echo "'$OJ_MARK'";?>;
         var user_id="<?php if (isset($_SESSION[$OJ_NAME."_user_id"])) echo $_SESSION[$OJ_NAME."_user_id"]; ?>";
 </script>
-        <script src="template/syzoj/auto_refresh.js?v=0.52" ></script>
+        <script src="template/<?php echo $OJ_TEMPLATE?>/auto_refresh.js?v=0.52" ></script>
 
 <?php include("template/$OJ_TEMPLATE/footer.php");?>
