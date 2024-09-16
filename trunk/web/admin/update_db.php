651fa9d04c8a589277a7a80f2e9a84d34d17af13
张浩斌 <newsclan@gmail.com>
2024-09-16 19:56:04 +0800

    Update update_db.php

--- trunk/web/admin/update_db.php
+++ trunk/web/admin/update_db.php
@@ -216,6 +216,8 @@ $tsql[49]="alter table $DB_NAME.problem add index key_p_def(defunct);";
 $csql[49]="alter table $DB_NAME.contest add index key_c_def(defunct);";
 $tsql[50]="alter table $DB_NAME.contest add index key_c_end(end_time);";
 $csql[50]="alter table $DB_NAME.contest add index key_c_dend(defunct,end_time);";
+$tsql[51]="alter table $DB_NAME.users add column starred int default 0 after activecode ;";
+$csql[51]="";
 // 删除6个月以前的非正确源码，优化数据库空间。
 // delete from source_code  where solution_id in (select solution_id from solution where result>4 and  in_date<date_sub(now(),interval 6 month) ); //
 if(isset($_POST['do'])){
