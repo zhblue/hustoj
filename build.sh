5cc702c65a773976d36f9ee932d4749a6cfe9614
张浩斌 <newsclan@gmail.com>
2024-09-19 20:27:24 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get install debhelper
+set -ex \
+&& git clone https://github.com/zhblue/hustoj.git \
+&& git clone https://github.com/zhblue/hustoj-deb-ubuntu.git \
+&& mv hustoj/trunk/* hustoj-deb-ubuntu \
+&& cd hustoj-deb-ubuntu && dpkg-buildpackage 
