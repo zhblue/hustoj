582e00ed8c6bdf15025fae8a42195b63bc620ed7
张浩斌 <newsclan@gmail.com>
2024-09-19 13:06:58 +0800

    Update auto_refresh.js

--- trunk/web/template/syzoj/auto_refresh.js
+++ trunk/web/template/syzoj/auto_refresh.js
@@ -79,10 +79,13 @@ function fresh_result(solution_id) {
                                 //console.log(ra[0]);
                                 switch (ra[0]) {
                                         case  4:
-                                                if(user_id==ra[5]) fancy(row.cells[4]);
-                                                //break;
                                         case 14:
-                                                row.cells[4].innerHTML += "<a href=reinfo.php?sid="+solution_id+" class='"+judge_color[ra[0]]+"'>"+judge_result[ra[0]]+"</a>";
+                                                if(user_id==ra[5]){
+                                                        fancy(row.cells[4]);
+                                                        row.cells[4].innerHTML += "<a href=reinfo.php?sid="+solution_id+" class='"+judge_color[ra[0]]+"'>"+judge_result[ra[0]]+"</a>";
+                                                }else{
+                                                        row.cells[4].innerHTML = "<a href=reinfo.php?sid="+solution_id+" class='"+judge_color[ra[0]]+"'>"+judge_result[ra[0]]+"</a>";
+                                                }
                                                 break;
                                         case 5:
                                         case 6:
