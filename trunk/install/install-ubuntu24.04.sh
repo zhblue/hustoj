89fae1a43de5d875a37fcdf30893b53957759f73
张浩斌 <newsclan@gmail.com>
2024-09-23 14:30:44 +0800

    Update install-ubuntu24.04.sh

--- trunk/install/install-ubuntu24.04.sh
+++ trunk/install/install-ubuntu24.04.sh
@@ -6,8 +6,10 @@ if [ -d /mnt/c ]; then
 #    exit 1
 fi
 MEM=`free -m|grep Mem|awk '{print $2}'`
+NBUFF=512
 if [ "$MEM" -lt "2000" ] ; then
         echo "Memory size less than 2GB."
+        NBUFF=128
         if grep 'swap' /etc/fstab ; then
                 echo "already has swap"
         else
@@ -154,7 +156,7 @@ else
         sed -i "s:}#added by hustoj::g" /etc/nginx/sites-enabled/default
         sed -i "s:php7.4:php$PHP_VER:g" /etc/nginx/sites-enabled/default
         sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
-        sed -i "s|fastcgi_pass 127.0.0.1:9000;|fastcgi_pass 127.0.0.1:9000;\n\t\tfastcgi_buffer_size 256k;\n\t\tfastcgi_buffers 32 64k;|g" /etc/nginx/sites-enabled/default
+        sed -i "s|fastcgi_pass 127.0.0.1:9000;|fastcgi_pass 127.0.0.1:9000;\n\t\tfastcgi_buffer_size 256k;\n\t\tfastcgi_buffers $NBUFF 64k;|g" /etc/nginx/sites-enabled/default
 fi
 /etc/init.d/nginx restart
 sed -i "s/post_max_size = 8M/post_max_size = 500M/g" /etc/php/$PHP_VER/fpm/php.ini
