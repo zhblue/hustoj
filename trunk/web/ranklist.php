d993aba595780ffabc87643b6e608e8a7921929a
张浩斌 <newsclan@gmail.com>
2024-10-16 08:29:33 +0800

    Update ranklist.php

--- trunk/web/ranklist.php
+++ trunk/web/ranklist.php
@@ -120,7 +120,7 @@
 
                         $view_rank[$i][0]= $rank;
                          $view_rank[$i][1]=  "<a href='userinfo.php?user=" .htmlentities ( $row['user_id'],ENT_QUOTES,"UTF-8") . "'>" . $row['user_id'] . "</a>";
-                        if(isset($row['starred']) && $row['starred'] >0 )   $view_rank[$i][1]="⭐".$view_rank[$i][1]."⭐";     //github starred rewarding
+                        if(isset($row['starred']) && $row['starred'] >0 )   $view_rank[$i][1]="⭐".$view_rank[$i][1]."<span title='用同名账户给hustoj项目加星，可以点亮此星' >⭐</span>";     //github starred rewarding
 			$view_rank[$i][2]=  "<div class=center>" . htmlentities ( $row['nick'] ,ENT_QUOTES,"UTF-8") ."</div>";
 			$view_rank[$i][3]=  "<div class=center>" . htmlentities ( $row['group_name'] ,ENT_QUOTES,"UTF-8") ."</div>";
                         $view_rank[$i][4]=  "<div class=center><a href='status.php?user_id=" .htmlentities ( $row['user_id'],ENT_QUOTES,"UTF-8") ."&jresult=4'>" . $row['solved']."</a>"."</div>";
