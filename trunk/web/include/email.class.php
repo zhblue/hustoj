5c25bf22a0ad555fa559794c0fd4d337ead21da9
张浩斌 <newsclan@gmail.com>
2024-10-17 10:01:58 +0800

    Update email.class.php

--- trunk/web/include/email.class.php
+++ trunk/web/include/email.class.php
@@ -17,7 +17,7 @@ function email($address,$mailtitle,$mailcontent,$html=""){
 	if( $SMTP_USER != "mailer@qq.com") {      // 不要修改这个检测标记
 		try {
 		    //Server settings
-		    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+		    $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
 		    $mail->isSMTP();                                            //Send using SMTP
 		    $mail->Host       = $SMTP_SERVER;                     //Set the SMTP server to send through
 		    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
