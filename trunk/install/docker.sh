0fd45ef6526dcea85fff1fdd0001c1a2965d0046
张浩斌 <newsclan@gmail.com>
2024-10-21 18:20:44 +0800

    Update docker.sh
    import with tagname

--- trunk/install/docker.sh
+++ trunk/install/docker.sh
@@ -37,7 +37,8 @@ then
   		echo "Download archive image file fail , try again..."
   	done
 	bzip2 -d hustoj.docker.tar.bz2
-	docker load < hustoj.docker.tar
+	#docker load < hustoj.docker.tar
+        docker import hustoj.docker.tar hustoj
 fi
  
 sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
