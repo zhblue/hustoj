2cfc5b91bd8713cc11a09cfab1a5e2a79890cb9c
张浩斌 <newsclan@gmail.com>
2024-11-26 09:00:30 +0800

    Update cnt.php

--- trunk/web/lang/cnt.php
+++ trunk/web/lang/cnt.php
@@ -177,7 +177,7 @@
  $MSG_LAST_LOGIN="最後登錄";
  $MSG_OFFLINE_ZIP_IMPORT="導入zip文件，遵循下面的目錄結構:";
  $MSG_OFFLINE="離線";
-
+ $MSG_EXPIRY_DATE="有效期";
  //contest.php
  $MSG_PRIVATE_WARNING ="比賽尚未開始或私有，不能查看題目。 ";
  $MSG_PRIVATE_USERS_ADD =" *可以將學生學號從Excel整列複製過來，然後要求他們用學號做UserID註冊,就能進入Private的比賽作為作業和測驗。 ";
