7e73de9864c3aa98aa8910f5d282e5847c5b1878
张浩斌 <newsclan@gmail.com>
2024-12-07 10:01:33 +0800

    Add files via upload

--- trunk/web/admin/user_import.php
+++ trunk/web/admin/user_import.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("display_errors", "On");  //set this to "On" for debugging  ,especially when no reason blank shows up.
 require_once ("admin-header.php");
 if (!(isset($_SESSION[$OJ_NAME . '_' . 'administrator']) || isset($_SESSION[$OJ_NAME . '_problem_importer']))) {
     echo "<a href='../loginpage.php'>Please Login First!</a>";
@@ -17,7 +18,9 @@ function get_extension($file) {
     return $info['extension'];
 }
 function import_user($filename) {
+    global $OJ_EXPIRY_DAYS,$MSG_EXPIRY_DATE;
     $check=false;
+    $expire=false;
     if (($h = fopen("{$filename}", "r")) !== FALSE) {
         // 文件中的每一行数据都被转换为我们调用的单个数组$data
         // 数组的每个元素以逗号分隔
@@ -27,27 +30,31 @@ function import_user($filename) {
                         $check=true;
                         echo "导入名单：<hr>\n";
                         $gb2312=false;
+			if(isset($data[6])) $expire=($data[6]==$MSG_EXPIRY_DATE );
                         continue;
-                }else if (iconv("gbk","utf-8",$data[0])=="学号") {
+                }else if (iconv("gb2312","utf-8",$data[0])=="学号") {
                         $check=true;
                         $gb2312=true;
+			$expire=(iconv("gb2312","utf-8",$data[6])==$MSG_EXPIRY_DATE );
                         continue;
                 }
             if($check){
-                    echo $data[0] . "<br>\n";
                     $user_id = mb_trim($data[0]);
                     $nick = $data[1];
                     $password = pwGen(trim($data[2]));
                     $school = "";
                     $email = "";
                     $group_name="";
+		    $expiry_date=addDays($OJ_EXPIRY_DAYS);
                     if (isset($data[3])) $school = $data[3];
                     if (isset($data[4])) $email = $data[4];
                     if (isset($data[5])) $group_name = $data[5];
+                    if (isset($data[6])) $expiry_date = $data[6];
                     if($gb2312) {
-                            $nick=iconv("gbk","utf-8",$nick);
-                            $school=iconv("gbk","utf-8",$school);
-                            $group_name=iconv("gbk","utf-8",$group_name);
+                            $nick=iconv("gb2312","utf-8",$nick);
+                            $school=iconv("gb2312","utf-8",$school);
+                            $group_name=iconv("gb2312","utf-8",$group_name);
+                            $expiry_date=iconv("gb2312","utf-8",$expiry_date);
                     }
                     if (mb_strlen($nick, 'utf-8') > 20) {
                         $new_len = mb_strlen($nick, 'utf-8');
@@ -59,8 +66,9 @@ function import_user($filename) {
                     }
 
                     $ip = "127.0.0.1";
-                    $sql = "INSERT INTO `users`(" . "`user_id`,`email`,`ip`,`accesstime`,`password`,`reg_time`,`nick`,`school`,`group_name`)" . "VALUES(?,?,?,NOW(),?,NOW(),?,?,?)on DUPLICATE KEY UPDATE `email`=?,`ip`=?,`accesstime`=NOW(),`password`=?,`reg_time`=now(),nick=?,`school`=?,`group_name`=?";
-                    pdo_query($sql, $user_id, $email, $ip, $password, $nick, $school,$group_name, $email, $ip, $password, $nick, $school,$group_name);
+                    $sql = "INSERT INTO `users`(" . "`user_id`,`email`,`ip`,`accesstime`,`password`,`reg_time`,`nick`,`school`,`group_name`,`expiry_date`)" . "VALUES(?,?,?,NOW(),?,NOW(),?,?,?,?)on DUPLICATE KEY UPDATE `email`=?,`ip`=?,`accesstime`=NOW(),`password`=?,`reg_time`=now(),nick=?,`school`=?,`group_name`=?,expiry_date=?";
+                    $ret=pdo_query($sql, $user_id, $email, $ip, $password, $nick, $school,$group_name,$expiry_date, $email, $ip, $password, $nick, $school,$group_name,$expiry_date);
+                    echo "$user_id : $expiry_date : $ret <br>\n";
 
             }else{
                 echo "<h1>请用下载的模板填写，保存为UTF-8编码。</h1>";
@@ -121,6 +129,7 @@ if (isset($_FILES["fps"])) {
       </center>
       <?php require_once ("../include/set_post_key.php"); ?>
     </form>
-<a href="users.csv">下载模板</a>
+<h2><a href="users.csv">下载模板</a></h2>
+<h3>请用下载的模板填写，保存为UTF-8编码。</h3>
 <?php
 } ?>
