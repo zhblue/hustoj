058d8a5777a1dc54d9228243df973175fa033cd4
张浩斌 <newsclan@gmail.com>
2024-12-08 10:30:06 +0800

    Update ajax.php

--- trunk/web/admin/ajax.php
+++ trunk/web/admin/ajax.php
@@ -1,30 +1,30 @@
 <?php
 require_once("../include/db_info.inc.php");
-if(!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator']))){
+if(!(isset($_SESSION[$OJ_NAME.'_administrator'])||isset($_SESSION[$OJ_NAME.'_problem_editor'])||isset($_SESSION[$OJ_NAME.'_contest_creator'])||isset($_SESSION[$OJ_NAME.'_tag_adder']))){
   echo "<a href='../loginpage.php'>Please Login First!</a>";
-  exit(1);
+  exit(1);  
 }
 if($_SERVER['REQUEST_METHOD']=="POST"){
 	$m=$_POST["m"];	
-	if($m=="problem_add_source"){
+	if($m=="problem_add_source" && ( isset($_SESSION[$OJ_NAME.'_administrator']) || isset($_SESSION[$OJ_NAME.'_problem_editor']) || isset($_SESSION[$OJ_NAME.'_tag_adder']) ) ){
 		$pid=intval($_POST['pid']);
 		$new_source=($_POST['ns']);	
 		$sql= "update problem set source=concat(source,' ',?) where problem_id=?";		
 		echo pdo_query($sql,$new_source,$pid);
 	}
-	if($m=="problem_update_time"){
+	if($m=="problem_update_time" && ( isset($_SESSION[$OJ_NAME.'_administrator']) || isset($_SESSION[$OJ_NAME.'_problem_editor']) ) ){
 		$pid=intval($_POST['pid']);
 		$time=intval($_POST['t']);	
 		$sql= "update problem set time_limit=? where problem_id=?";		
 		echo pdo_query($sql,$time,$pid);
 	}
-	if($m=="problem_get_title"){
+	if($m=="problem_get_title"  && ( isset($_SESSION[$OJ_NAME.'_administrator']) || isset($_SESSION[$OJ_NAME.'_problem_editor']) )){
                 $pid=intval($_POST['pid']);
                 $sql= "select title,source from problem where problem_id=?";
                 $row=mysql_query_cache($sql,$pid)[0];
                 echo $row['title']."&nbsp;&nbsp;<span class='label label-success'>".$row['source']."</span>";
 	}
-        if($m=="user_update_nick"){
+        if($m=="user_update_nick"  && ( isset($_SESSION[$OJ_NAME.'_administrator']) )){
                 $user_id=$_POST['user_id'];
                 $nick=$_POST['nick'];
                 $sql= "update users set nick=? where user_id=?";
@@ -32,25 +32,25 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 		$sql= "update solution set nick=? where user_id=?";
                 pdo_query($sql,$nick,$user_id);
         }
-	if($m=="user_update_expiry_date"){
+	if($m=="user_update_expiry_date"  && ( isset($_SESSION[$OJ_NAME.'_administrator']) )){
                 $user_id=$_POST['user_id'];
                 $expiry_date=$_POST['expiry_date'];
                 $sql= "update users set expiry_date=? where user_id=?";
                 echo pdo_query($sql,$expiry_date,$user_id);
         }
-	if($m=="user_update_school"){
+	if($m=="user_update_school"  && ( isset($_SESSION[$OJ_NAME.'_administrator']) )){
                 $user_id=$_POST['user_id'];
                 $school=$_POST['school'];
                 $sql= "update users set school=? where user_id=?";
                 echo pdo_query($sql,$school,$user_id);
         }
-	if($m=="user_update_group_name"){
+	if($m=="user_update_group_name"  && ( isset($_SESSION[$OJ_NAME.'_administrator']) )){
                 $user_id=$_POST['user_id'];
                 $group_name=$_POST['group_name'];
                 $sql="update users set group_name=? where user_id=?";
                 echo pdo_query($sql,$group_name,$user_id);
         }
-	if($m=="get_user_list_of_contest"){
+	if($m=="get_user_list_of_contest"  && ( isset($_SESSION[$OJ_NAME.'_administrator'])||isset($_SESSION[$OJ_NAME.'_contest_creator']) )){
 			$contest_id=$_POST['contest_id'];
 			$sql= "select distinct user_id from privilege where rightstr=? ";
 			$users=pdo_query($sql,"c".$contest_id);
