d785398faf5064fc90b29674de50d0ea7e5366e1
张浩斌 <newsclan@gmail.com>
2024-12-09 17:58:06 +0800

    Update init.php

--- trunk/web/include/init.php
+++ trunk/web/include/init.php
@@ -1,6 +1,7 @@
 <?php
 // 启用 XSS 保护，仅对同源资源启用
 header("X-XSS-Protection: 1; mode=block; sameorigin");
+header("X-Download-Options: noopen");
 
 require_once(dirname(__FILE__)."/pdo.php");
 require_once(dirname(__FILE__)."/memcache.php");
