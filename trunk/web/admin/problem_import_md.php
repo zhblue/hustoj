290970124b7d50c2d3bce4f16b746dfd9bb22a72
Tiang-88 <51110833+Tiang-88@users.noreply.github.com>
2024-12-10 20:36:28 +0800

    Update problem_import_md.php
    导入std cpp和测试数据

--- trunk/web/admin/problem_import_md.php
+++ trunk/web/admin/problem_import_md.php
@@ -140,6 +140,8 @@ function process_md_and_images($zip_path, $upload_dir)
         $zip->close();
 
         $md_files = glob($temp_dir . "/*.md");
+         $cpp_files = glob($temp_dir . "/*.cpp");
+        $zip_files = glob($temp_dir . "/*.zip"); // 获取所有 .zip 文件
         $output_files = [];
 
         foreach ($md_files as $md_file) {
@@ -171,6 +173,23 @@ function process_md_and_images($zip_path, $upload_dir)
             ];
         }
 
+
+        // Process .cpp files
+        foreach ($cpp_files as $cpp_file) {
+            $output_files[] = [
+                'filename' => basename($cpp_file),
+                'content' => file_get_contents($cpp_file)
+            ];
+        }
+
+        // Process .zip files
+        foreach ($zip_files as $zip_file) {
+            $output_files[] = [
+                'filename' => basename($zip_file),
+                'content' => file_get_contents($zip_file)
+            ];
+        }
+
         // Cleanup the temp directory
         array_map('unlink', glob("$temp_dir/*.*"));
         rmdir($temp_dir);
@@ -181,56 +200,100 @@ function process_md_and_images($zip_path, $upload_dir)
     }
 }
 
-if ($_FILES ["fps"] ["error"] > 0) {
-  echo "&nbsp;&nbsp;- Error: ".$_FILES ["fps"] ["error"]."File size is too big, change in PHP.ini<br />";
-}
-else {
-  $tempdir = sys_get_temp_dir()."/import_markdown".time();	
-  mkdir($tempdir, 0755, true); // 确保目录存在并可写
-  $tempfile = $_FILES ["fps"] ["tmp_name"];
-  if (get_extension( $_FILES ["fps"] ["name"])=="zip") {
-    echo "&nbsp;&nbsp;- zip file, only Markdown .md file is supported<hr>\n";
+if ($_FILES["fps"]["error"] > 0) {
+    echo "&nbsp;&nbsp;- Error: " . $_FILES["fps"]["error"] . " File size is too big, change in PHP.ini<br />";
+} else {
+    $tempdir = sys_get_temp_dir() . "/import_markdown" . time();
+    mkdir($tempdir, 0777, true);
+    $tempfile = $_FILES["fps"]["tmp_name"];
+    if (get_extension($_FILES["fps"]["name"]) == "zip") {
+        echo "&nbsp;&nbsp;- zip file, only Markdown .md file is supported<hr>\n";
 
-    $upload_dir = "/home/judge/src/web/upload/image";
-    $output_files = process_md_and_images($tempfile, $upload_dir);
+        $upload_dir = "/home/moniti/src/web/upload/image";
+        $output_files = process_md_and_files($tempfile, $upload_dir);
 
-    foreach ($output_files as $file) {
-        $hydrop=explode("\n",$file['content']);	
-        $title=str_replace("#","",$hydrop[0]);	
-        $title=str_replace("\r","",$title);	
-        $source="";	
-        echo "<hr>".htmlentities($file['filename']." $title $source");
-        $regex = '/<(?!div)/';
-        $regex = '/(?<!div)>\s?/';
-        $description="<span class=\"md\">".$file['content']."</span>";
-        $description=preg_replace('/{{ select\(\d+\) }}/', "", $description); 
-        $tail=0;
-        $ptitle = $title;
-        while (hasProblem($ptitle)) {
-          $tail++;
-          $ptitle = $title."_".$tail;
+        
+        $cpp_files = []; // 保存所有 .cpp 文件
+        $zip_files = []; // 保存所有 .zip 文件
+        foreach ($output_files as $file) {
+            if (pathinfo($file['filename'], PATHINFO_EXTENSION) == 'cpp') {
+                $cpp_files[basename($file['filename'], ".cpp")] = $file; // 按基文件名存储
+            } elseif (pathinfo($file['filename'], PATHINFO_EXTENSION) == 'zip') {
+                $zip_files[basename($file['filename'], ".zip")] = $file; // 按基文件名存储
+            }
         }
-        $title=$ptitle;
 
-        // 初始化变量
-        $input = "";
-        $output = "";
-        $sample_input = "";
-        $sample_output = "";
-        $hint = "";
-        $spj = 0;
+        foreach ($output_files as $file) {
+            if (pathinfo($file['filename'], PATHINFO_EXTENSION) == 'md') {
+                $hydrop = explode("\n", $file['content']);
+                $title = str_replace("#", "", $hydrop[0]);
+                $title = str_replace("\r", "", $title);
+                $source = "";
+                echo "<hr>" . htmlentities($file['filename'] . " $title $source");
+                $description = "<span class=\"md\">" . $file['content'] . "</span>";
+                $description = preg_replace('/{{ select\(\d+\) }}/', "", $description);
+                $tail = 0;
+                $ptitle = $title;
+                while (hasProblem($ptitle)) {
+                    $tail++;
+                    $ptitle = $title . "_" . $tail;
+                }
+                $title = $ptitle;
 
-        $pid = addproblem($title,1,128, $description, $input, $output, $sample_input, $sample_output, $hint, $source, $spj, $OJ_DATA);
-        if (!file_exists($OJ_DATA."/$pid/")) {
-            mkdir($OJ_DATA."/$pid/", 0755, true);
+                // 初始化变量
+                $input = "";
+                $output = "";
+                $sample_input = "";
+                $sample_output = "";
+                $hint = "";
+                $spj = 0;
+
+                // 添加题目并获取pid
+                $pid = addproblem($title, 1, 128, $description, $input, $output, $sample_input, $sample_output, $hint, $source, $spj, $OJ_DATA);
+                if (!file_exists($OJ_DATA . "/$pid/")) {
+                    mkdir($OJ_DATA . "/$pid/", 0777, true);
+                }
+                echo "PID:<a href='../problem.php?id=$pid' >" . htmlentities($title, ENT_QUOTES, "UTF-8") . "</a>";
+
+                
+
+                // 根据基文件名匹配对应的 .cpp 文件，并重命名为 题目编号-std.cpp
+                if (isset($cpp_files[$basename])) {
+                    $cpp_file_path = "$OJ_DATA/$pid/$pid-std.cpp";
+                    file_put_contents($cpp_file_path, $cpp_files[$basename]['content']);
+                    echo "<br>Uploaded solution file: $pid-std.cpp to $pid folder.";
+                }
+
+                // 根据基文件名匹配对应的 .zip 文件，并解压到 $OJ_DATA/$pid/ 目录下
+                if (isset($zip_files[$basename])) {
+                    $zip_file_path = "$OJ_DATA/$pid/" . $zip_files[$basename]['filename'];
+                    file_put_contents($zip_file_path, $zip_files[$basename]['content']);
+
+                    $zip = new ZipArchive();
+                    if ($zip->open($zip_file_path) === TRUE) {
+                        // 解压到目标目录，忽略内部目录结构
+                        for ($i = 0; $i < $zip->numFiles; $i++) {
+                            $entry = $zip->getNameIndex($i);
+                            $entry_path = "$OJ_DATA/$pid/" . basename($entry);
+                            copy("zip://" . $zip_file_path . "#" . $entry, $entry_path);
+                        }
+                        $zip->close();
+                        echo "<br>Extracted test data: " . $zip_files[$basename]['filename'] . " to $pid folder.";
+
+                        // 删除 ZIP 文件
+                        unlink($zip_file_path);
+                        echo "<br>Deleted ZIP file: " . $zip_files[$basename]['filename'];
+                    } else {
+                        echo "<br>Failed to extract test data: " . $zip_files[$basename]['filename'];
+                    }
+                }
+            }
         }
-        echo "PID:<a href='../problem.php?id=$pid' >".htmlentities($title,ENT_QUOTES,"UTF-8")."</a>";
+
+        unlink($_FILES["fps"]["tmp_name"]);
+        rmdir($tempdir);
+    } else {
+        echo "&nbsp;&nbsp;- Only zip files are supported<br />";
     }
-
-    unlink ( $_FILES ["fps"] ["tmp_name"] );
-    rmdir ($tempdir);
-  } else {
-    echo "&nbsp;&nbsp;- Only zip files are supported<br />";
-  }
 }
 ?>
