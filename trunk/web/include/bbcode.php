4f2c08c7a757bacf6ba0cc77e075c0bc3d6b2cad
张浩斌 <newsclan@gmail.com>
2025-01-04 16:57:46 +0800

    Update bbcode.php

--- trunk/web/include/bbcode.php
+++ trunk/web/include/bbcode.php
@@ -175,7 +175,7 @@ class BBCode
         // OPENING TAG
 
         // Big if / elseif ladder to handle each tag
-        if ($name === 'b' || $name === 'u' || $name === 's' || $name === 'sup' || $name === 'sub' ||
+        if ($name === 'b' || $name === 'u'  || $name === 'sup' || $name === 'sub' ||     // $name === 's'
             $name === 'blockquote' ||
             $name === 'ol' || $name === 'ul' ||
             $name === 'table') {
