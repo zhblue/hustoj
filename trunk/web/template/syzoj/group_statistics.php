719558940d7e220ecf35f57f780130db1a260c3e
张浩斌 <newsclan@gmail.com>
2025-01-07 20:38:34 +0800

    Update group_statistics.php

--- trunk/web/template/syzoj/group_statistics.php
+++ trunk/web/template/syzoj/group_statistics.php
@@ -61,8 +61,41 @@ if(!empty($result)){
       </tbody>
 </table>
 <?php } ?>
-
+<button id="swapButton">矩阵转置/行列转换</button>
 </center>
+<script>
+        function setScale(scale) {
+            $(".label.mini").css("transform",`scale(${scale})`);
+        }
+
+$(document).ready(function() {
+    $('#swapButton').click(function() {
+        var originalTable = $('#statistics');
+        var newTable = $('<table id=statistics class="ui table striped"></table>');
+
+        // Get all rows from the original table
+        var rows = originalTable.find('tr');
+        var row_count=rows.length;
+        var col_count = $(rows[0]).find("th,td").length;
+        console.log("row_count:"+row_count);
+        console.log("col_count:"+col_count);
+        // Create a new row for each column
+        for(let i=0;i<col_count;i++){
+                var newRow = $('<tr></tr>');
+                for(let index=0;index<row_count;index++) {
+                        // Create a new cell and add it to the new row
+                    let cols=$(rows[index]).find('th,td');
+                    let html=$(cols[i]).html();
+                    var newCell = $('<td></td>').html(html);
+                    newRow.append(newCell);
+                }
+                newTable.append(newRow);
+        }
+        originalTable.replaceWith(newTable);
+    });
+});
+</script>
+
 <?php include("template/$OJ_TEMPLATE/footer.php");?>
 <script src="<?php echo $OJ_CDN_URL?>include/sortTable.js"></script>
       <script>
