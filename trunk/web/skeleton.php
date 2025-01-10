73869f54965a0050ee8f5aef86776abe1034e444
张浩斌 <newsclan@gmail.com>
2025-01-10 21:34:54 +0800

    Update skeleton.php

--- trunk/web/skeleton.php
+++ trunk/web/skeleton.php
@@ -9,8 +9,14 @@ require_once( './include/bbcode.php' );
 $view_title = "Hello skeleton";
 /////////////////////////Template
 
+$sql="select count(1) cnt from users where defunct=? ";
+$result=pdo_query($sql,"N" );
+if(!empty($result))
+	$cnt=$result[0]['cnt'];
 
 $view_content="这是一个骨架页，如果您想二次开发一个自己的页面，可以分别在web目录和template/syzoj目录，复制两个skeleton.php到新的文件名，`cp skeleton.php myfile.php` ，然后开始修改自己的myfile.php";
+$view_content.="<>当前有效用户: $cnt 人";
+
 require( "template/" . $OJ_TEMPLATE . "/".basename(__FILE__));
 /////////////////////////Common foot
 if ( file_exists( './include/cache_end.php' ) )
