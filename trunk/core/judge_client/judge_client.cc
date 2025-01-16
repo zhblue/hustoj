0371dca8eac2b672c64fc75aafbfbd452dd71d1d
张浩斌 <newsclan@gmail.com>
2025-01-16 18:58:34 +0800

    Update judge_client.cc

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1350,7 +1350,7 @@ void _update_user_mysql(char *user_id)
 {
 	char sql[BUFFER_SIZE];
 	sprintf(sql,
-			"UPDATE `users` SET `solved`=(SELECT count(DISTINCT `problem_id`) FROM `solution` WHERE `user_id`=\'%s\' AND `result`=4) WHERE `user_id`=\'%s\'",
+			"UPDATE `users` SET `solved`=(SELECT count(DISTINCT `problem_id`) FROM `solution` s left join contest c on s.`user_id`=\'%s\' AND s.`result`=4 and s.contest_id=c.contest_id and s.problem_id>0 and c.contest_type & 20 = 0 ) WHERE `user_id`=\'%s\'",
 			user_id, user_id);
 	if (mysql_real_query(conn, sql, strlen(sql)))
 		write_log(mysql_error(conn));
