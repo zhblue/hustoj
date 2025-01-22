eecdda9a8b3a33caba7fdfc5474a35513dd1e872
张浩斌 <newsclan@gmail.com>
2025-01-22 10:40:33 +0800

    Update db_info.inc.php

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -101,7 +101,7 @@ static  $OJ_REMOTE_JUDGE=false; //是否启用Remote Judge ，启用哪些模块
 static  $OJ_NO_CONTEST_WATCHER=false ; //是否禁止无权限用户观战私有比赛
 static  $OJ_CONTEST_TOTAL_100=false; //是否让比赛按100分计分
 static  $OJ_OLD_FASHINED=false; //是否在状态页的编辑按钮、管理页的预览模式等方面保留原始版本的习惯。
-static  $OJ_AI_HTML=false; // 若想开启AI链接，可设为 '<a class="desktop-only item active" onclick="window.open(\'http://ai.hustoj.com\', \'_blank\',\'width=600,height=850,left=\' + (window.screen.width-600)  + \',top=0,toolbar=no,menubar=no,location=no,status=no,resizable=yes\');"><i class="help icon"></i> 问问狗蛋</a>';
+static  $OJ_AI_HTML=false; // 若想开启AI链接，可设为 '<a class="desktop-only item" onclick="window.open(\'http://ai.hustoj.com\', \'_blank\',\'width=600,height=850,left=\' + (window.screen.width-600)  + \',top=0,toolbar=no,menubar=no,location=no,status=no,resizable=yes\');"><i class="help icon"></i> 问问狗蛋</a>';
 static  $OJ_PUBLIC_STATUS=true; //是否公开所有人的判题结果,设为false则除source_browser外，其他人只能看到自己提交的记录。
 static  $OJ_FANCY_RESULT=false; //是否在AC时显示fancy.php里的动画
 static  $OJ_FANCY_MP3='http://cdn.hustoj.com/mp3.php'; // 答案正确时的音效
