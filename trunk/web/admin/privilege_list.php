112427dd94d14e432483efb15bf46ddd6ff3ac0d
张浩斌 <newsclan@gmail.com>
2025-01-23 10:44:09 +0800

    Update privilege_list.php

--- trunk/web/admin/privilege_list.php
+++ trunk/web/admin/privilege_list.php
@@ -75,7 +75,7 @@ if(isset($_GET['keyword']) && $_GET['keyword']!=""){
 	if($row['rightstr']!="administrator"  ||$row['user_id']!=$_SESSION[$OJ_NAME."_user_id"])
         	echo "<td><a href='privilege_delete.php?uid=".htmlentities($row['user_id'],ENT_QUOTES,"UTF-8")."&rightstr={$row['rightstr']}&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey']."'>Delete</a></td>";
 	else
-		echo "Can't suicide";
+		echo "<td>Can't suicide</td>";
 	echo "</tr>";
     }
     ?>
