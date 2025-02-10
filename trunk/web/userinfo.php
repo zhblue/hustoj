00f9c2b1a8a5e00cb91a198c7a502aba334c1dcf
张浩斌 <newsclan@gmail.com>
2025-02-10 08:41:28 +0800

    Update userinfo.php

--- trunk/web/userinfo.php
+++ trunk/web/userinfo.php
@@ -85,7 +85,7 @@ if(!empty($bible)){
 $exceptions=array();
 if(isset($OJ_NOIP_KEYWORD)&&$OJ_NOIP_KEYWORD && !isset($_SESSION[$OJ_NAME."_administrator"])){  // && !isset($_SESSION[$OJ_NAME."_administrator"])   管理员不受限
 		                     $now =  date('Y-m-d H:i', time());
-				     $sql="select contest_id from contest c where  c.start_time<'$now' and c.end_time>'$now' and ( c.title like '%$OJ_NOIP_KEYWORD%' or (c.contest_type & 20) >0 )";
+				     $sql="select contest_id from contest c where  c.start_time<'$now' and c.end_time>'$now' and ( c.title like '%$OJ_NOIP_KEYWORD%' or ((c.contest_type & 20) >0 and end_time>now() ) )";
 		                     $row=pdo_query($sql);
 				     if(count($row)>0){
 				        $exceptions=array_column($row,'contest_id');
