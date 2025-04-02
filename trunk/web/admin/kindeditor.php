5f0e443f06ee5b6e66365c119f028719cb47e3ac
张浩斌 <newsclan@gmail.com>
2025-04-02 09:01:56 +0800

    Add files via upload

--- trunk/web/admin/kindeditor.php
+++ trunk/web/admin/kindeditor.php
@@ -31,6 +31,51 @@ if(!kindeditorSeted){
                                         K.ctrl(self.edit.doc, 13, function() {
                                                 self.sync();
                                         });
+					        var editerDoc = this.edit.doc;//得到编辑器的文档对象
+						//监听粘贴事件, 包括右键粘贴和ctrl+v
+						$(editerDoc).bind('paste', null, function (e) {
+						    var ele = e.originalEvent.clipboardData.items;
+						    for (var i = 0; i < ele.length; ++i) {
+							//判断文件类型
+							if ( ele[i].kind == 'file' && ele[i].type.indexOf('image/') !== -1 ) {
+							    var file = ele[i].getAsFile();//得到二进制数据
+							    //创建表单对象，建立name=value的表单数据。
+							    var formData = new FormData();
+							    formData.append("imgFile",file);//name,value
+
+							    //用jquery Ajax 上传二进制数据
+							    $.ajax({
+								url : '/kindeditor/php/upload_json.php?dir=image',
+								type : 'POST',
+								data : formData,
+								// 告诉jQuery不要去处理发送的数据
+								processData : false,
+								// 告诉jQuery不要去设置Content-Type请求头
+								contentType : false,
+								dataType:"json",
+								beforeSend:function(){
+								    //console.log("正在进行，请稍候");
+								},
+								success : function(responseStr) {
+								    //上传完之后，生成图片标签回显图片，假定服务器返回url。
+								    var src = responseStr.url;
+								    var imgTag = "<img src='"+src+"' width='486px' border='0'/>";
+
+								    //console.info(imgTag);
+								    //kindeditor提供了一个在焦点位置插入HTML的函数，调用此函数即可。
+								    editor1.insertHtml(imgTag);
+
+
+								},
+								error : function(responseStr) {
+								    console.log("error");
+								}
+							    });
+
+							}
+
+						    }
+						});
                                 }
                                 ,
                                         afterBlur: function() {
