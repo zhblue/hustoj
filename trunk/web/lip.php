a0fa77def5f8dc83f21325c582b7cb3b0df2c1e0
张浩斌 <newsclan@gmail.com>
2025-04-05 11:48:32 +0800

    Update lip.php

--- trunk/web/lip.php
+++ trunk/web/lip.php
@@ -1,4 +1,5 @@
 <?php
+// 在校园网、局域网中部署此文件，用于检测和记录客户端的局域网IP地址。
 $ip = ($_SERVER['REMOTE_ADDR']??"");
 if( isset($_SERVER['HTTP_X_FORWARDED_FOR'] )&&!empty( trim( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) ){
     $REMOTE_ADDR = $_SERVER['HTTP_X_FORWARDED_FOR'];
@@ -10,3 +11,4 @@ if( isset($_SERVER['HTTP_X_FORWARDED_FOR'] )&&!empty( trim( $_SERVER['HTTP_X_FOR
     $ip =(htmlentities($tmp_ip[0],ENT_QUOTES,"UTF-8"));
 }
 header("location:http://your.public.domain.com/session.php?lip=".ip2long($ip) );
+// 修改上面的域名、服务地址为你的公网服务器地址。
