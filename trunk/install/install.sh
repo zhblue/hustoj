9e709555ef222c38366754a260ff0f10e214b7fe
张浩斌 <newsclan@gmail.com>
2025-04-16 07:13:13 +0800

    Update install.sh

--- trunk/install/install.sh
+++ trunk/install/install.sh
@@ -49,7 +49,10 @@ DATABASE=`cat $config|grep 'OJ_DB_NAME' |awk -F= '{print $2}'`
 PORT=`cat $config|grep 'OJ_PORT_NUMBER' |awk -F= '{print $2}'`
 IP=`curl http://hustoj.com/ip.php`
 LIP=`ip a|grep inet|grep brd|head -1|awk '{print $2}'|awk -F/ '{print $1}'`
-
+cd /home/judge/src/web/
+wget dl.hustoj.com/hello.tar.gz
+tar xzf hello.tar.gz
+chown www-data -R hello
 clear
 reset
 
