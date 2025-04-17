99178724cb84b436347c31c721c0f083febe2503
张浩斌 <newsclan@gmail.com>
2025-04-17 11:38:14 +0800

    Update install-ubuntu22.04.gitee.sh

--- trunk/install/install-ubuntu22.04.gitee.sh
+++ trunk/install/install-ubuntu22.04.gitee.sh
@@ -144,7 +144,7 @@ if grep "added by hustoj" /etc/nginx/sites-enabled/default ; then
 else
         echo "modify the default site"
         sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
-        sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
+        sed -i "s:index index.html:index index.php index.html:g" /etc/nginx/sites-enabled/default
         sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
         sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
         sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
