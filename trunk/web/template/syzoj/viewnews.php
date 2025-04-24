9f6a02fff30b75c5276dd2e28198633c9148885a
张浩斌 <newsclan@gmail.com>
2025-04-24 17:02:32 +0800

    Update viewnews.php

--- trunk/web/template/syzoj/viewnews.php
+++ trunk/web/template/syzoj/viewnews.php
@@ -12,5 +12,38 @@
         <div id="content" class="font-content"><?php echo bbcode_to_html($news_content)?></div>
     </div>
 </div>
+<script>
+  $(document).ready(function(){
+                marked.use({
+                  // 开启异步渲染
+                  async: true,
+                  pedantic: false,
+                  gfm: true,
+                  mangle: false,
+                  headerIds: false
+                });
+                $(".md").each(function(){
+                        $(this).html(marked.parse($(this).text()));
+                });
+                // adding note for ```input1  ```output1 in description
+                for(let i=1;i<10;i++){
+                        $(".language-input"+i).parent().before("<div><?php echo $MSG_Input?>"+i+":</div>");
+                        $(".language-output"+i).parent().before("<div><?php echo $MSG_Output?>"+i+":</div>");
+                }
+        $(".md table tr td").css({
+            "border": "1px solid grey",
+            "text-align": "center",
+            "width": "200px",
+            "height": "30px"
+        });
 
+        $(".md table th").css({
+            "border": "1px solid grey",
+            "width": "200px",
+            "height": "30px",
+            "background-color": "#9e9e9ea1",
+            "text-align": "center"
+        });
+  });
+</script>
 <?php include("template/$OJ_TEMPLATE/footer.php");?>
