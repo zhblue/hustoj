b1854d0bc607beb0118983084597339aec14132c
张浩斌 <newsclan@gmail.com>
2025-05-03 11:13:18 +0800

    Create tpj.cc

---
+++ trunk/install/tpj.cc
@@ -0,0 +1,20 @@
+// clang-format off
+
+#include "testlib.h"
+#include <cmath>
+
+int main(int argc, char *argv[]) {
+  /*
+   * inf：输入
+   * ouf：选手输出
+   * ans：标准输出
+   */
+  registerTestlibCmd(argc, argv);
+
+  double pans = ouf.readDouble(), jans = ans.readDouble();
+
+  if (abs(pans - jans) < 1e-3)
+    quitf(_ok, "Good job\n");
+  else
+    quitf(_wa, "Too big or too small, expected %f, found %f\n", jans, pans);
+}
