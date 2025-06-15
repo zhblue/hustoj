1baf75d9a13a6a0d79037b17cfc672f0ada06a7b
张浩斌 <newsclan@gmail.com>
2025-06-15 18:40:56 +0800

    Update online.php

--- trunk/web/template/syzoj/online.php
+++ trunk/web/template/syzoj/online.php
@@ -34,7 +34,7 @@
 		<tr>
 			<td><?php $l = $location->getlocation($u['ip']);
                                 echo $u['ip'].'<br />';
-                                echo $l['country'].$l['area']??"";
+                                echo $l??"";
                             ?>
                         </td>
 			<td><?php echo $u['uri']?></td>
