3957f31f4ad6fa7fafc153e16effbae9bbdd9dc5
张浩斌 <newsclan@gmail.com>
2025-11-03 09:25:12 +0800

    Fix formatting issues in ranklist.php

--- trunk/web/ranklist.php
+++ trunk/web/ranklist.php
@@ -3,7 +3,7 @@
         $cache_time=30;
         require_once('./include/cache_start.php');
     	require_once('./include/db_info.inc.php');
-	require_once("./include/my_func.inc.php");
+	    require_once("./include/my_func.inc.php");
         require_once('./include/setlang.php');
         require_once('./include/memcache.php');
         $now =  date('Y-m-d H:i', time());
@@ -138,4 +138,5 @@ require("template/".$OJ_TEMPLATE."/ranklist.php");
 if(file_exists('./include/cache_end.php'))
         require_once('./include/cache_end.php');
 ?>
+
 
