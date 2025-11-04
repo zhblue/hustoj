a9e03c7cbb40bec700632959edf8902a93a59a3a
张浩斌 <newsclan@gmail.com>
2025-11-05 07:26:15 +0800

    Add QWEN_API_KEY for API integration

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -104,6 +104,7 @@ static  $OJ_AI_HTML=false; // 若想开启AI链接，可设为 '<a class="deskto
 static  $OJ_PUBLIC_STATUS=true; //是否公开所有人的判题结果,设为false则除source_browser外，其他人只能看到自己提交的记录。
 static  $OJ_FANCY_RESULT=false; //是否在AC时显示fancy.php里的动画
 static  $OJ_FANCY_MP3='http://cdn.hustoj.com/mp3.php'; // 答案正确时的音效
+static  $QWEN_API_KEY="sk-更换成你自己的Qwen API Key"; // 登录阿里云，打开 https://bailian.console.aliyun.com/?tab=model#/api-key
 
 //static  $OJ_EXAM_CONTEST_ID=1000; // 启用考试状态，填写考试比赛ID，比赛开始前可以随便坐，机器有问题及时更换座位，比赛开始后IP锁定，只能从最后一次登录的位置登录。更换机器需要管理员后台手工指定IP。
 //static  $OJ_ON_SITE_CONTEST_ID=1000; //启用现场赛状态，填写现场赛比赛ID，全系统其他功能禁用，自动跳转指定编号的比赛。
