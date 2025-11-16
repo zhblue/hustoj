206c2826659f3206203143c192957ae9a3b9cc59
张浩斌 <newsclan@gmail.com>
2025-11-16 11:40:27 +0800

    Update and rename trunk/web/bigmodel.php to trunk/web/aiapi/bigmodel.php

--- trunk/web/aiapi/bigmodel.php
+++ trunk/web/aiapi/bigmodel.php
@@ -1,6 +1,6 @@
 <?php
 // 这个文件用于对接智普bigmodel.cn，解析编译报错和运行错误信息。
-// 需要在 db_info.inc.php 文件中配置 $QWEN_API_KEY
+// 需要在 db_info.inc.php 文件中配置 $OJ_AI_API_URL
 // 注册邀请 智谱 GLM Coding 超值订阅，邀你一起薅羊毛！Claude Code、Cline 等 10+ 大编程工具无缝支持，“码力”全开，越拼越爽！立即开拼，享限时惊喜价！
 //
 // 链接：https://www.bigmodel.cn/claude-code?ic=YVNXOQOZX5
@@ -9,7 +9,11 @@
 // 注意这个功能可能会导致付费账单，
 // 访问类似 https://bigmodel.cn/finance-center/resource-package/package-mgmt
 // 关注所用模型的剩余免费额度
-require_once("include/db_info.inc.php");
+require_once("../include/db_info.inc.php");
+
+// 请用智普API Key将下行 $QWEN_API_KEY ;
+$apiKey = "填写你申请的api-key";
+
 $sid=intval($_GET['sid']);
 $user_id=pdo_query("select user_id from solution where solution_id=?",$sid)[0][0];
 if(!(isset($_SESSION[$OJ_NAME."_source_browser"])|| $user_id==$_SESSION[$OJ_NAME."_user_id"] )){
@@ -50,8 +54,7 @@ if(!empty($answer)){
 
 // 设置请求的URL
 $url = 'https://open.bigmodel.cn/api/paas/v4/chat/completions';
-// 若没有配置环境变量，请用智普API Key将下行 $QWEN_API_KEY ;
-$apiKey = "$QWEN_API_KEY";
+
 // 设置请求头
 $headers = [
     'Authorization: Bearer '.$apiKey,
