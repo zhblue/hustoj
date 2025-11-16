a211df0838c5a1c4cd4e6b477651503ab13b1055
张浩斌 <newsclan@gmail.com>
2025-11-16 17:32:50 +0800

    Update qwen.php

--- trunk/web/aiapi/qwen.php
+++ trunk/web/aiapi/qwen.php
@@ -1,11 +1,15 @@
 <?php
 // 这个文件用于对接阿里千问，解析编译报错和运行错误信息。
-// 需要在db_info.inc.php中配置 $QWEN_API_KEY;  
+// 需要在db_info.inc.php中配置 $OJ_AI_API_URL指向本文件; 
 // 登录阿里云，打开 https://bailian.console.aliyun.com/?tab=model#/api-key  创建新的API KEY
 // 注意这个功能可能会导致阿里云付费账单，
 // 访问类似 https://bailian.console.aliyun.com/?tab=model#/model-market/detail/qwen3-coder-480b-a35b-instruct
 // 关注所用模型的剩余免费额度
 require_once("../include/db_info.inc.php");
+$apiKey = "设置为阿里云的API-KEY";   //https://bailian.console.aliyun.com/?tab=model#/api-key  创建新的API KEY
+$models=array("qwen-turbo","qwen3-coder-480b-a35b-instruct","qwen3-max","qwen3-coder-30b-a3b-instruct");
+
+
 $sid=intval($_GET['sid']);
 $solution=pdo_query("select user_id,problem_id from solution where solution_id=?",$sid)[0];
 $user_id=$solution[0];
@@ -50,7 +54,7 @@ $problem=pdo_query("select concat(description,'输入:',input,'输出:',output,'
 // 设置请求的URL
 $url = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
 // 若没有配置环境变量，请用百炼API Key将下行替换为：$apiKey = "sk-xxx";
-$apiKey = $QWEN_API_KEY;
+
 // 设置请求头
 $headers = [
     'Authorization: Bearer '.$apiKey,
@@ -61,7 +65,7 @@ if(isset($_SESSION[$OJ_NAME."_source_browser"])){
 }else{
 	$code_suggestion="不要直接给出完整代码,只给出问题原因,让我自己学习修改。分析我可能薄弱的知识点，问我一个提示性的相关问题。";
 }
-$models=array("qwen-turbo","qwen3-coder-480b-a35b-instruct","qwen3-max","qwen3-coder-30b-a3b-instruct");
+
 $model = $models[array_rand($models)];
 // 设置请求体
 $data = [
