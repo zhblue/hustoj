e5f28e623c595073a193bababa2e9e6806f024a6
张浩斌 <newsclan@gmail.com>
2025-12-04 19:47:59 +0800

    Add AI Help message to Russian language file

--- trunk/web/lang/ru.php
+++ trunk/web/lang/ru.php
@@ -480,6 +480,7 @@
        $MSG_OTHERS="Other's ";
        $MSG_SUBNET="Subnet ";
 $MSG_ONLY_LAST_SUBMISSION="Only the last submission will be scored";
+	   $MSG_AI_HELP="AI Help";
        $MSG_SHOW_DIFF="Show Differ Output";
        $MSG_UPSOLVING="Upsolving";
 //SaaS friendly
