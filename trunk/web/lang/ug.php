6405affc2922e54d6ef32300c9e0adbb03f0b874
张浩斌 <newsclan@gmail.com>
2025-12-04 19:54:31 +0800

    Update ug.php

--- trunk/web/lang/ug.php
+++ trunk/web/lang/ug.php
@@ -479,6 +479,7 @@ $MSG_TOO_BAD="Please surf the internet in a civilized manner";
        $MSG_OTHERS="Other's ";
        $MSG_SUBNET="Subnet ";
        $MSG_ONLY_LAST_SUBMISSION="Only the last submission will be scored";
+	   $MSG_AI_HELP="AI Help";
        $MSG_SHOW_DIFF="Show Differ Output";
        $MSG_UPSOLVING="Upsolving";
 //SaaS friendly
