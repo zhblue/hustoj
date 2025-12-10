df9eec0870b079992c234017b6a8da345b14d3cf
张浩斌 <newsclan@gmail.com>
2025-12-10 15:44:30 +0800

    Update github.php

--- trunk/web/aiapi/github.php
+++ trunk/web/aiapi/github.php
@@ -2,7 +2,9 @@
 // 参考此文 to https://docs.github.com/zh/github-models/quickstart
 // 这个文件用于对接github-models，解析编译报错和运行错误信息。
 // 需要在db_info.inc.php中配置 $OJ_AI_API_URL指向本文件;
-// 登录github，打开  https://github.com/settings/personal-access-tokens/new  创建新的API KEY
+// 登录github，打开  
+//  https://github.com/settings/personal-access-tokens/new?description=Used+to+call+GitHub+Models+APIs+to+easily+run+LLMs%3A+https%3A%2F%2Fdocs.github.com%2Fgithub-models%2Fquickstart%23step-2-make-an-api-call&name=GitHub+Models+token&user_models=read  创建新的API KEY
+// 创建新的API KEY
 // 注意这个功能可能会导致付费账单，
 // 访问类似 https://docs.github.com/zh/github-models/use-github-models/prototyping-with-ai-models#rate-limits
 // 关注所用模型的速率限制
