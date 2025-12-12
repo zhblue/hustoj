d49a7b917014ad1b6d070d669cb8e63672388e4f
张浩斌 <newsclan@gmail.com>
2025-12-12 18:55:34 +0800

    Update upload_json.php

--- trunk/web/kindeditor/php/upload_json.php
+++ trunk/web/kindeditor/php/upload_json.php
@@ -6,7 +6,6 @@
  * 如果您确定直接使用本程序，使用之前请仔细确认相关安全设置。
  *
  */
-@session_start();
 require_once("../../include/db_info.inc.php");
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])
       ||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])
@@ -168,3 +167,4 @@ function alert($msg) {
 	json_encode(array('error' => 1, 'message' => $msg));
 	exit;
 }
+
