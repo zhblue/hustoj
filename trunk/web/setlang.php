a7489f71a7accdb8822f53488e69aaa30dcce38c
张浩斌 <newsclan@gmail.com>
2025-12-13 16:40:37 +0800

    Update setlang.php

--- trunk/web/setlang.php
+++ trunk/web/setlang.php
@@ -1,7 +1,6 @@
 <?php
 require_once("include/db_info.inc.php");
 if (isset($_GET['lang']) && in_array($_GET['lang'], array("cn","ug", "en", 'fa', 'ko', 'th'))) {
-    session_start();
     $_SESSION[$OJ_NAME . '_' . 'OJ_LANG'] = $_GET['lang'];
     setcookie("lang", $_GET['lang'], time() + 604800);
 }
