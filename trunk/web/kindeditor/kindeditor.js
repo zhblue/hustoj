039c732295f31567164dc0a039b5804cb97cb54b
张浩斌 <newsclan@gmail.com>
2025-12-17 14:21:43 +0800

    Update kindeditor.js

--- trunk/web/kindeditor/kindeditor.js
+++ trunk/web/kindeditor/kindeditor.js
@@ -17,7 +17,7 @@ if (!window.console) {
 if (!console.log) {
 	console.log = function () {};
 }
-var _VERSION = '4.1.7 (2013-06-08)',
+var _VERSION = '4.1.7 (2025-05-29)',
 	_ua = navigator.userAgent.toLowerCase(),
 	_IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
 	_GECKO = _ua.indexOf('gecko') > -1 && _ua.indexOf('khtml') == -1,
