cbd4ca08bd60f1366eba956133aaaea83b82b2ce
张浩斌 <newsclan@gmail.com>
2025-12-19 08:57:21 +0800

    Update memcache.php
    首先检测系统是否支持APCu缓存扩展，然后定义mysql_query_cache()函数，该函数通过对SQL语句和参数进行MD5哈希生成缓存键，如果APCu可用则优先使用APCu进行缓存查询，命中缓存直接返回结果，未命中则执行实际查询并缓存结果（有效期10秒）；如果APCu不可用则回退到memcache缓存方案（通过getCache/setCache函数），若memcache也未配置则会提示安装建议。最终返回查询结果，从而减少重复数据库查询、提升系统性能。

--- trunk/web/include/memcache.php
+++ trunk/web/include/memcache.php
@@ -26,34 +26,41 @@
         global $memcache;
         return ($memcache) ? $memcache->set($key,$object,MEMCACHE_COMPRESSED,$timeout) : false;
     }
-
-    # Caching version of pdo_query()
-    function mysql_query_cache($sql){ 
-	global $OJ_NAME,$OJ_MEMCACHE;	
+    $OJ_APCU_OK = ( extension_loaded('apcu') && apcu_enabled() );
+    function mysql_query_cache($sql){
+        global $OJ_NAME,$OJ_MEMCACHE,$OJ_APCU_OK;
     $linkIdentifier = false;
-    $timeout = 60;
-//首先调用上面的getCache函数，如果返回值不为false的话，就说明是从memcached服务器获取的数据
-//如果返回false，此时就需要直接从数据库中获取数据了。
-//需要注意的是这里使用操作的命令加上sql语句的md5码作为一个特定的key，可能大家觉得使用数据项的
-//名称作为key会比较自然一点。运行memcached加上"-vv"参数，并且不作为daemon运行的话，可以看见
-//memcached处理时输出的相关信息
+    $timeout = 5;
+	//首先调用上面的getCache函数，如果返回值不为false的话，就说明是从memcached服务器获取的数据
+	//如果返回false，此时就需要直接从数据库中获取数据了。
+	//需要注意的是这里使用操作的命令加上sql语句的md5码作为一个特定的key，可能大家觉得使用数据项的
+	//名称作为key会比较自然一点。运行memcached加上"-vv"参数，并且不作为daemon运行的话，可以看见
+	//memcached处理时输出的相关信息
     $num_args = func_num_args();
     $args = func_get_args();       //获得传入的所有参数的数组
     $args = array_slice($args,1,--$num_args);
     $key=md5($OJ_NAME.$_SERVER['HTTP_HOST']."mysql_query" . $sql.implode(" ",$args));
-        if (!($cache = getCache($key))) {
+        if($OJ_APCU_OK){   // 如果php内置的apcu可用，优先用apcu缓存
+            $apcu_success=false;
+            $cache=apcu_fetch($key,$apcu_success);
+            if($apcu_success){
+                //echo "-APCU cache-";
+                return $cache;
+            }else{
+                $cache =pdo_query($sql,$args);
+                apcu_store($key,$cache,$timeout);
+            }
+        }else if (!($cache = getCache($key))) {
             $cache = false;
             $cache =pdo_query($sql,$args);
-
-    //将数据放入memcached服务器中，如果memcached服务器没有开的话，此语句什么也不会做
-    //如果开启了服务器的话，数据将会被缓存到memcached服务器中
+        //将数据放入memcached服务器中，如果memcached服务器没有开的话，此语句什么也不会做
+        //如果开启了服务器的话，数据将会被缓存到memcached服务器中
                 if (!setCache($key, $cache, $timeout)) {
                     # If we get here, there isn’t a memcache daemon running or responding
-		 if($OJ_MEMCACHE) echo "You can run these command to get faster speed:<br>sudo apt-get install memcached<br>sudo apt-get install php5-memcache<br>sudo apt-get install php-memcache";
+                 if($OJ_MEMCACHE) echo "You can run these command to get faster speed:<br>sudo apt-get install memcached<br>sudo apt-get install php5-memcache<br>sudo apt-get install php-memcache";
                 }
 
         }
-	
         return $cache;
     }
-?>
+
