92709f767765f9ffe4f27828c918a369796fc495
张浩斌 <newsclan@gmail.com>
2025-12-24 16:58:47 +0800

    Update recent-contest.php

--- trunk/web/recent-contest.php
+++ trunk/web/recent-contest.php
@@ -6,8 +6,7 @@ require_once( './include/cache_start.php' );
 require_once( './include/db_info.inc.php' );
 require_once( './include/memcache.php' );
 require_once( './include/setlang.php' );
-require( "template/" . $OJ_TEMPLATE . "/recent-contest.php" );
+require("template/" . $OJ_TEMPLATE . "/" . basename(__FILE__));
 /////////////////////////Common foot
 if ( file_exists( './include/cache_end.php' ) )
 	require_once( './include/cache_end.php' );
-?>
