b8e95485a8bd1441541bd2aaa113179fe63c44cd
张浩斌 <newsclan@gmail.com>
2025-12-24 16:30:04 +0800

    Update modifypage.php

--- trunk/web/modifypage.php
+++ trunk/web/modifypage.php
@@ -1,25 +1,33 @@
-<?php	$cache_time=10;
-	$OJ_CACHE_SHARE=false;
-	require_once('./include/cache_start.php');
-    require_once('./include/db_info.inc.php');
-	require_once('./include/setlang.php');
-	$view_title= "Welcome To Online Judge";
-	if (!isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
-		$view_errors= "<a href=./loginpage.php>$MSG_Login</a>";
-		
-		require("template/".$OJ_TEMPLATE."/error.php");
-		exit(0);
-	}
+<?php 
+// 设置缓存时间为10秒
+$cache_time = 10;
+// 设置OJ缓存共享为false
+$OJ_CACHE_SHARE = false;
+require_once('./include/cache_start.php');
+require_once('./include/db_info.inc.php');
+require_once('./include/setlang.php');
+// 设置页面标题为"Welcome To Online Judge"
+$view_title = "Welcome To Online Judge";
 
-$sql="SELECT `school`,`nick`,`email` FROM `users` WHERE `user_id`=?";
-$result=pdo_query($sql,$_SESSION[$OJ_NAME.'_'.'user_id']);
- $row=$result[0];
+// 检查用户是否已登录，如果未登录则跳转到登录页面
+if (!isset($_SESSION[$OJ_NAME . '_' . 'user_id'])) {
+    $view_errors = "<a href=./loginpage.php>$MSG_Login</a>";
+    require("template/" . $OJ_TEMPLATE . "/error.php");
+    exit(0);
+}
+
+// 从数据库中查询当前用户的学校、昵称和邮箱信息
+$sql = "SELECT `school`,`nick`,`email` FROM `users` WHERE `user_id`=?";
+$result = pdo_query($sql, $_SESSION[$OJ_NAME . '_' . 'user_id']);
+$row = $result[0];
 
 
 /////////////////////////Template
-require("template/".$OJ_TEMPLATE."/modifypage.php");
+// 加载修改页面模板
+require("template/" . $OJ_TEMPLATE . "/modifypage.php");
 /////////////////////////Common foot
-if(file_exists('./include/cache_end.php'))
-	require_once('./include/cache_end.php');
-?>
+// 如果存在缓存结束文件则包含该文件
+if (file_exists('./include/cache_end.php'))
+    require_once('./include/cache_end.php');
+
 
