5c42864d2a44272c053bbf7563ce420cf1385887
张浩斌 <newsclan@gmail.com>
2025-12-24 14:22:39 +0800

    Update bbs.php

--- trunk/web/bbs.php
+++ trunk/web/bbs.php
@@ -3,57 +3,44 @@
 <?php
 require_once("./include/db_info.inc.php");
 
-if($OJ_BBS===false || (isset($OJ_EXAM_CONTEST_ID) && $OJ_EXAM_CONTEST_ID>0))
-{
-  header("Content-type: text/html; charset=utf-8");
-  echo $MSG_BBS_NOT_ALLOWED_FOR_EXAM;
-  exit ();
+if ($OJ_BBS === false || (isset($OJ_EXAM_CONTEST_ID) && $OJ_EXAM_CONTEST_ID > 0)) {
+    header("Content-type: text/html; charset=utf-8");
+    echo $MSG_BBS_NOT_ALLOWED_FOR_EXAM;
+    exit ();
 }
 
 $parm = "";
 
-if(isset($_GET['pid']))
-{
-  $pid = intval($_GET['pid']);
-  $parm = "pid=".$pid;
-}
-else
-{
-  $pid = 0;
+if (isset($_GET['pid'])) {
+    $pid = intval($_GET['pid']);
+    $parm = "pid=" . urlencode($pid);
+} else {
+    $pid = 0;
 }
 
-if(isset($_GET['cid']))
-{
-  $cid = intval($_GET['cid']);
-  $parm .= "&cid=".$cid;
-}
-else
-{
-  $cid = 0;
-}
-
-if($OJ_BBS == "discuss")
-{
-  echo ("<script>location.href='discuss/discuss.php?".$parm."';</script>");
-}
-else if($OJ_BBS == "discuss3")
-{
-  if($parm == "")
-    echo ("<script>location.href='discuss.php';</script>");
-  else
-    echo ("<script>location.href='discuss.php?".$parm."';</script>");
-}
-else
-{
-  if(isset($_GET['pid']))
-  {
-    $url = ("bbs/search.php?fid[]=2&keywords=".$pid); //chenge this to your own phpBB search link
-  }
-  else
-  {
-    $url = ("bbs/");
-  }
-  echo ("<script>location.href='".$url."';</script>");
+if (isset($_GET['cid'])) {
+    $cid = intval($_GET['cid']);
+    if ($parm !== "") {
+        $parm .= "&";
+    }
+    $parm .= "cid=" . urlencode($cid);
+} else {
+    $cid = 0;
 }
 
-?>
+if ($OJ_BBS == "discuss") {
+    $redirect_url = 'discuss/discuss.php?' . $parm;
+    echo("<script>location.href='" . addslashes($redirect_url) . "';</script>");
+} else if ($OJ_BBS == "discuss3") {
+    $redirect_url = $parm == "" ? 'discuss.php' : 'discuss.php?' . $parm;
+    echo("<script>location.href='" . addslashes($redirect_url) . "';</script>");
+} else {
+    if (isset($_GET['pid'])) {
+        $url = ("bbs/search.php?fid[]=2&keywords=" . urlencode($pid)); //chenge this to your own phpBB search link
+    } else {
+        $url = ("bbs/");
+    }
+    echo("<script>location.href='" . addslashes($url) . "';</script>");
+}
+
+
