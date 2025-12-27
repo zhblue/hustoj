a5165bc33e9cecc446c48b335f9287ace7a0d083
张浩斌 <newsclan@gmail.com>
2025-12-27 09:14:04 +0800

    Update post.php

--- trunk/web/post.php
+++ trunk/web/post.php
@@ -2,7 +2,28 @@
 require_once("discuss_func.inc.php");
 require_once("include/db_info.inc.php");
 require_once("include/my_func.inc.php");
+if(!isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
+  $view_errors="<a href=loginpage.php>Please Login First</a>";
+  require("template/".$OJ_TEMPLATE."/error.php");
+  exit(0);
+}
+if (!$OJ_BBS || $OJ_BBS != "discuss3") {
+    header("location: ../index.php");
+    exit();
+}
+if (isset($OJ_EXAM_CONTEST_ID) && $OJ_EXAM_CONTEST_ID > 0) {
+    header("Content-type: text/html; charset=utf-8");
+    echo $MSG_BBS_NOT_ALLOWED_FOR_EXAM;
+    exit ();
 
+}
+if (isset($_GET['tid']) && !isset($_GET['cid'])) {
+    $tid = intval($_GET['tid']);
+    $cid = pdo_query("select cid from topic where tid=?", $tid)[0][0];
+    if ($cid > 0) $_GET['cid'] = $cid;
+//              echo "cid:".$cid;
+
+}
 if (!isset($_SESSION[$OJ_NAME . '_' . 'user_id'])) {
     $view_errors = "<a href=loginpage.php>Please Login First</a>";
     require("template/" . $OJ_TEMPLATE . "/error.php");
@@ -100,3 +121,4 @@ if ($_REQUEST['action'] == 'reply' || !is_null($tid)) {
         exit(0);
     }
 }
+
