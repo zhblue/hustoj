212195b7098aa5bacf63b465281f1d82df0ffe79
张浩斌 <newsclan@gmail.com>
2025-12-27 21:24:39 +0800

    Add user registration via curl in setup.sh
    Added a curl command to register a user in the setup script.

--- docker/setup.sh
+++ docker/setup.sh
@@ -110,6 +110,21 @@ if ps -C memcached; then
     sed -i 's/static  $OJ_MEMCACHE=false;/static  $OJ_MEMCACHE=true;/g' /home/judge/src/web/include/db_info.inc.php
 fi
 
+curl 'http://127.0.0.1/register.php' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'DNT: 1' \
+  -H 'Origin: http://demo.hustoj.com' \
+  -H 'Referer: http://demo.hustoj.com/registerpage.php' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0' \
+  -b cookie \
+  --data-raw 'user_id=admin&nick=admin&password=test2025&rptpassword=test2025&school=hustoj&email=10982766c%40qq.com&submit=&csrf=nHdjh9za5dKfjJwSB1siuRe28pwbdSBl' \
+  --insecure
+
 for page in `cat test.lst` 
 do 
 	if w3m -dump http://127.0.0.1/$page |grep HUSTOJ > /dev/null ; then 
