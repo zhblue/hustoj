2666f6fd0b650c5eb0e96d97bee7c3aa615d7880
张浩斌 <newsclan@gmail.com>
2026-01-01 12:45:18 +0800

    Update API URL and add comments for clarity

--- trunk/web/aiapi/demo.php
+++ trunk/web/aiapi/demo.php
@@ -9,11 +9,11 @@
 require_once("../include/db_info.inc.php");
 require_once("../include/my_func.inc.php");
 // 设置请求的URL
-$url = 'http://demo.hustoj.com/aiapi/proxy.php';
+$url = 'http://demo.hustoj.com/aiapi/proxy.php';   // 千问是：'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
 $apiKey = "设置为阿里云的API-KEY";   //https://bailian.console.aliyun.com/?tab=model#/api-key  创建新的API KEY
 $models=array("qwen-turbo","qwen3-coder-480b-a35b-instruct","qwen3-max","qwen3-coder-30b-a3b-instruct");
-$temperature=0.8;
 
+$temperature=0.8;
 $http_referer =basename(parse_url( $_SERVER['HTTP_REFERER'])['path']);
 if((isset($_SESSION[$OJ_NAME.'_administrator'])|| isset($_SESSION[$OJ_NAME.'_problem_editor']) ) ){
        if(str_starts_with( basename($http_referer),"phpfm.php")|| str_starts_with( basename($http_referer),"submitpage.php") ){
