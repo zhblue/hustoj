cba10899fc7edd7b513e2588b131ef6b9298bf4e
shiningrise <shiningrise@qq.com>
2026-01-02 14:06:58 +0800

    debug docker镜像

--- trunk/docker/hustoj/build-dokcer.sh
+++ trunk/docker/hustoj/build-dokcer.sh
@@ -8,7 +8,7 @@ docker rm -f /hustoj-dev
 #chmod -R 777 /home/test/
 
 #docker run -d -it --privileged --name hustoj-dev -p 80:80 hustoj-dev
-docker run -d -it --privileged --name hustoj-dev -p 80:80 -v /home/test/:/data hustoj-dev
+docker run -d -it --privileged --name hustoj -p 8080:80 -v /data/hustoj/:/data shiningrise/hustoj:20260102
 #docker run -d -it --name hustoj-dev -p 80:80 hustoj-dev
 docker exec -i -t hustoj-dev /bin/bash
 
@@ -16,3 +16,6 @@ SELECT user, host FROM mysql.user;
 CREATE USER 'hustoj'@'localhost' IDENTIFIED BY 'hustoj';
 CREATE USER 'hustoj'@'%' IDENTIFIED BY 'hustoj';
 FLUSH PRIVILEGES;
+
+docker build -f Dockerfile -t hustoj  ./
+docker build -f Dockerfile --no-cache -t hustoj  ./
