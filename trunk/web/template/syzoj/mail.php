7e76e0c8fa353710cace0d5a011a3fc1c66e0c57
张浩斌 <newsclan@gmail.com>
2026-01-21 08:43:34 +0800

    Update mail.php

--- trunk/web/template/syzoj/mail.php
+++ trunk/web/template/syzoj/mail.php
@@ -4,15 +4,16 @@
   <i class="mail icon"></i>
   <div class="content">
 <?php
-if($view_content)
-echo "<center>
-<table>
-<tr>
-<td class=blue>$from_user -&gt; $to_user [".htmlentities(str_replace("\n\r","\n",$view_title),ENT_QUOTES,"UTF-8")." ]</td>
-</tr>
-<tr><td><pre>". htmlentities(str_replace("\n\r","\n",$view_content),ENT_QUOTES,"UTF-8")."</pre>
-</td></tr>
-</table></center>";
+if($view_content){
+    echo "<center>
+    <table>
+    <tr>
+    <td class=blue>$from_user -&gt; $to_user [".htmlentities(str_replace("\n\r","\n",$view_title),ENT_QUOTES,"UTF-8")." ]</td>
+    </tr>
+    <tr><td><pre>". htmlentities(str_replace("\n\r","\n",$view_content),ENT_QUOTES,"UTF-8")."</pre>
+    </td></tr>
+    </table></center>";
+}
 ?>
 <table><form method=post action=mail.php>
         <tr><td>From:<?php echo htmlentities($from_user,ENT_QUOTES,"UTF-8")?>
