7d427feafaa74a3ca9e992de289c8d1813a2449b
jepeng <jepeng2019@qq.com>
2026-02-13 01:06:58 +0800

    fix(admin): 修复公告编辑中的未定义键警告与 Header 已发送错误
    - 增加对 $_POST['showInMenu'] 的 isset 检查，解决未勾选复选框时的警告。
    - 引入 ob_start() 并将 header 跳转改为 JS alert + location 方式，彻底解决 "headers already sent" 问题。
    - 优化代码结构，将数据查询逻辑移至 kindeditor 包含之前。
    - 清理 textarea 渲染时的多余空白字符。

--- trunk/web/admin/news_edit.php
+++ trunk/web/admin/news_edit.php
@@ -1,80 +1,101 @@
 <?php
+ob_start();
 require_once("admin-header.php");
 if(!(isset($_SESSION[$OJ_NAME.'_'.'administrator']))){
   echo "<a href='../loginpage.php'>Please Login First!</a>";
   exit(1);
 }
 
-require_once("../include/db_info.inc.php");
-require_once("../include/my_func.inc.php");
-
 echo "<hr>";
-echo "<center><h3>".$MSG_NEWS."-"."Edit"."</h3></center>";
+echo "<center><h3>".$MSG_NEWS."-".$MSG_ADD."</h3></center>";
 
 include_once("kindeditor.php");
 ?>
 
-<div class="padding">
 <?php
-if(isset($_POST['news_id'])){
-  require_once("../include/check_post_key.php");
-
-  $title = $_POST['title'];
-  $content = $_POST['content'];
-  $showInMenu = $_POST['showInMenu'];
-  $menu = $showInMenu == "on" ? 1 : 0;
-
-  $content = str_replace("<p>", "", $content);
-  $content = str_replace("</p>", "<br />", $content);
-//  $content = str_replace(",", "&#44;", $content);
-
-  $user_id = $_SESSION[$OJ_NAME.'_'.'user_id'];
-  $news_id = intval($_POST['news_id']);
-
-  $sql = "UPDATE `news` SET `title`=?,`time`=now(),`content`=?,user_id=?,`menu`=? WHERE `news_id`=?";
-  //echo $sql;
-  pdo_query($sql,$title,$content,$user_id,$menu,$news_id);
-  $sessionDataKey = $OJ_NAME.'_'."_MENU_NEWS_CACHE";
-  unset($_SESSION[$sessionDataKey]);
-  header("location:news_list.php");
-  exit();
-}else{
-  $news_id = intval($_GET['id']);
-  $sql = "SELECT * FROM `news` WHERE `news_id`=?";
-  $result = pdo_query($sql,$news_id);
-  if(count($result)!=1){
-    echo "No such News!";
-    exit(0);
-  }
-
+if(isset($_GET['cid'])){
+  $cid = intval($_GET['cid']);
+  $sql = "SELECT * FROM news WHERE `news_id`=?";
+  $result = pdo_query($sql,$cid);
   $row = $result[0];
-
-  $title = htmlentities($row['title'],ENT_QUOTES,"UTF-8");
+  $title = $row['title'];
   $content = $row['content'];
-  $showInMenu = $row['menu'] == 1;
+  $defunct = $row['defunct'];
+}
+$plist = "";
+if(isset($_POST['pid'])){
+	sort($_POST['pid']);
+	foreach($_POST['pid'] as $i){
+	  if($plist)
+	    $plist.=','.intval($i);
+	  else
+	    $plist = $i;
+	}
+	
+  if(isset($_POST['hlist']))$plist = trim($_POST['hlist']);
+  $pieces = explode(",",$plist );
+  $pieces = array_unique($pieces);
+  if($pieces[0]=="") unset($pieces[0]);
+  $plist=implode(",",$pieces);
+
+	  $content="[plist=".$plist."]".htmlentities($_POST['keyword'],ENT_QUOTES,"utf-8")."[/plist]";
 }
 ?>
 
-  <form method=POST action=news_edit.php>
-    <input type=hidden name='news_id' value=<?php echo $news_id?>>
+<div class="padding">
+  <form method=POST action=news_add.php>
     <p align=left>
       <label class="col control-label"><?php echo $MSG_TITLE?></label>
-      <input type=text name=title size=71 value='<?php echo $title?>'>
+	  <input class="input input-large" style="width:100%;" size=71 value='<?php echo isset($title)?$title."-Copy":""?>' type=text name='title' id='title' > 
+	  <input type=submit class='btn btn-success' value='<?php echo $MSG_SAVE?>' name=submit> 
+	  <input class='btn btn-primary' id='ai_bt' type=button value='AI一下' onclick='ai_gen()' >
+	  <input class='btn btn-danger'  type=reset value='<?php echo $MSG_RESET?>' onclick='setTimeout("ai_gen()",500);' >
     </p>
     <p align=left>
       <label class="col control-label"><?php echo $MSG_NEWS_MENU?>
-        <input style="display: inline-block;" type="checkbox" name=showInMenu <?php if($showInMenu) { echo "checked"; } ?> />
+        <input style="display: inline-block;" type="checkbox" name=showInMenu />
       </label>
     </p>
     <p align=left>
-      <textarea class=kindeditor name=content rows=41 ><?php echo htmlentities($content,ENT_QUOTES,"UTF-8")?>
+      <textarea class=kindeditor name=content rows=41 >
+        <?php echo isset($content)?$content:""?>
       </textarea>
     </p>
-    <?php require_once("../include/set_post_key.php");?>
     <p>
       <center>
       <input type=submit value='<?php echo $MSG_SAVE?>' name=submit>
       </center>
     </p>
+    <?php require_once("../include/set_post_key.php");?>
   </form>
 </div>
+<script>
+
+	function ai_gen(filename){
+		    let oldval=$('#ai_bt').val();
+		    $('#ai_bt').val('AI思考中...请稍候...');
+		    $('#ai_bt').prop('disabled', true);;
+		    let title=$('#title').val();
+		    $.ajax({
+		    	url: '../<?php echo $OJ_AI_API_URL?>', 
+			type: 'GET',
+			data: { title: title },
+			success: function(data) {
+			    console.log(title);
+			    if(title==""){
+				    $('#title').val(data);
+			    }else{
+				    let description="<span class='md'>"+(data)+"</span>";
+				    let preview=$("#previewFrame").contents();
+				    $("textarea").eq(0).val(description); // 假设 #file_data 是 div
+			    }
+		    	    $('#ai_bt').prop('disabled', false);;
+			    $('#ai_bt').val('AI一下');
+			},
+			error: function() {
+			    $('#ai_bt').val('获取数据失败');
+		    	    $('#ai_bt').prop('disabled', false);;
+			}
+		    });
+	}
+</script>
