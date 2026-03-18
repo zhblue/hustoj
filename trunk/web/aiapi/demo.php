736cfc8f8a236a3c8f00684962fa5b64a18779bd
张浩斌 <newsclan@gmail.com>
2026-03-18 15:33:22 +0800

    Update demo.php for AI functionality demonstration

--- trunk/web/aiapi/demo.php
+++ trunk/web/aiapi/demo.php
@@ -1,6 +1,6 @@
 <?php
 //ini_set("display_errors", "On");  //set this to "On" for debugging  ,especially when no reason blank shows up.
-// 这个文件用于对接阿里千问，解析编译报错和运行错误信息。
+// 这个文件用于演示AI功能，解析编译报错和运行错误信息。
 // 需要在db_info.inc.php中配置 $OJ_AI_API_URL指向本文件; 
 // 登录阿里云，打开 https://bailian.console.aliyun.com/?tab=model#/api-key  创建新的API KEY
 // 注意这个功能可能会导致阿里云付费账单，
@@ -11,7 +11,7 @@ require_once("../include/my_func.inc.php");
 // 设置请求的URL
 $url = 'http://demo.hustoj.com/aiapi/proxy.php';   // 千问是：'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
 $apiKey = "设置为阿里云的API-KEY";   //https://bailian.console.aliyun.com/?tab=model#/api-key  创建新的API KEY
-$models=array("qwen-turbo","qwen3-coder-480b-a35b-instruct","qwen3-max","qwen3-coder-30b-a3b-instruct");
+$models=array("qwen-turbo");
 $temperature=0.8;
 if(basename(get_included_files()[0])!="cron.php")
   require_once(dirname(__FILE__)."/common.php");
