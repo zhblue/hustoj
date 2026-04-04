zhblue/hustoj

commit 0b60e6d532c339782210bde8d5c16b138c986523

fix XSS: add htmlentities to all direct $_SESSION echo outputs
- template/*: sidebar, bs3, bshark, sweet, mdui - header, submitpage, status, conteststatus, problem, problemset, modifypage, problem1, problem2
- admin/: contest_list, news_list, phpfm, problem_list, rejudge, privilege_add, user_list
- root: thread, newpost, export_ac_code, include/set_post_key
- Fixed getkey parameters and user_id outputs in JS strings, HTML text, URL params
- csrf.php class attribute output (low risk, boolean)

trunk/web/newpost.php
@@ -34,7 +34,7 @@ if (isset($_GET['cid'])) {
             <td width='5%' class='center'></td>
             <td width='15%' class='hidden-xs center'>
                 <?php
-                echo "<a href='../userinfo.php?user={$_SESSION[$OJ_NAME.'_'.'user_id']}'>{$_SESSION[$OJ_NAME.'_'.'user_id']}</a>";
+                echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'">".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
                 ?>
             </td>
             <td colspan=2>

trunk/web/template/sidebar/header.php
@@ -179,12 +179,12 @@
             <?php echo $sql_news_menu_result_html; ?>
             <div class="right menu">
                 <?php if(isset($_SESSION[$OJ_NAME.'_'.'user_id'])) { ?>
-                <a href="<?php echo $path_fix?>/userinfo.php?user=<?php echo $_SESSION[$OJ_NAME.'_'.'user_id']?>"
+                <a href="<?php echo $path_fix?>/userinfo.php?user=<?php echo htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')?>"
                     style="color: inherit; ">
                     <div class="ui simple dropdown item">
-                        <?php echo $_SESSION[$OJ_NAME.'_'.'user_id']; 
-                              if(!empty($_SESSION[$OJ_NAME.'_nick'])) echo "(".$_SESSION[$OJ_NAME.'_nick'].")";
-                              if(!empty($_SESSION[$OJ_NAME.'_group_name'])) echo "[".$_SESSION[$OJ_NAME.'_group_name']."]";
+                        <?php echo htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8'); 
+                              if(!empty($_SESSION[$OJ_NAME.'_nick'])) echo "(".htmlentities($_SESSION[$OJ_NAME.'_nick'], ENT_QUOTES, 'UTF-8').")";
+                              if(!empty($_SESSION[$OJ_NAME.'_group_name'])) echo "[".htmlentities($_SESSION[$OJ_NAME.'_group_name'], ENT_QUOTES, 'UTF-8')."]";
                                       
                         ?>
                         <i class="dropdown icon"></i>
@@ -193,7 +193,7 @@
                             <a class="item" href="portal.php"><i class="tasks icon"></i><?php echo $MSG_TODO;?></a>
                                 <?php if ($OJ_SaaS_ENABLE){ ?>
                                 <?php if($_SERVER['HTTP_HOST']==$DOMAIN)
-                                        echo  "<a class='item' href='http://".  $_SESSION[$OJ_NAME.'_'.'user_id'].".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
+                                        echo  "<a class='item' href='http://".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8']).".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
                                 <?php } ?>
                             <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])){ ?>
                             <a class="item" href="admin/"><i class="settings icon"></i><?php echo $MSG_ADMIN;?></a>

trunk/web/template/sidebar/problem1.php
@@ -57,7 +57,7 @@
         require_once("include/set_get_key.php");
       ?>
 
-      [<a href="admin/problem_edit.php?id=<?php echo $id?>&getkey=<?php echo $_SESSION[$OJ_NAME.'_'.'getkey']?>" >Edit</a>]
+      [<a href="admin/problem_edit.php?id=<?php echo $id?>&getkey=<?php echo htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8')?>" >Edit</a>]
       [<a href='javascript:phpfm(<?php echo $row['problem_id'];?>)'>TestData</a>]
 
     <?php
@@ -118,7 +118,7 @@
       require_once("include/set_get_key.php");
     ?>
 
-    [<a href="admin/problem_edit.php?id=<?php echo $id?>&getkey=<?php echo $_SESSION[$OJ_NAME.'_'.'getkey']?>" >Edit</a>]
+    [<a href="admin/problem_edit.php?id=<?php echo $id?>&getkey=<?php echo htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8')?>" >Edit</a>]
     [<a href='javascript:phpfm(<?php echo $row['problem_id'];?>)'>TestData</a>]
 
   <?php

trunk/web/thread.php
@@ -154,7 +154,7 @@ $isadmin = isset($_SESSION[$OJ_NAME . '_' . 'administrator']);
                 </td>
                 <td width='15%' class='center'>
                     <?php
-                    echo "<a href='../userinfo.php?user={$_SESSION[$OJ_NAME.'_'.'user_id']}'>{$_SESSION[$OJ_NAME.'_'.'user_id']}</a>";
+                    echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'">".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
                     ?>
                 </td>
                 <td colspan=2>

trunk/web/template/mdui/_includes/header.php
@@ -8,7 +8,7 @@
         <div class="mdui-btn" mdui-tooltip="{content: '账户'}" mdui-menu="{target: '#account-menu', position: 'bottom', fixed: true}">
             <i class="mdui-icon material-icons">account_circle</i>
             <?php if(isset($_SESSION[$OJ_NAME.'_'.'user_id'])) { ?>
-                <?php echo $_SESSION[$OJ_NAME.'_'.'user_id']; ?>
+                <?php echo htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8'); ?>
             <?php } else { ?>
                 请登录
             <?php } ?>

trunk/web/admin/contest_list.php
@@ -84,16 +84,16 @@ if(isset($_GET['keyword']) && $_GET['keyword']!=""){
       echo "<td>".$row['end_time']."</td>";
       $cid = $row['contest_id'];
       if(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'."m$cid"])){
-        echo "<td><a href=contest_pr_change.php?cid=".$row['contest_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey'].">".($row['private']=="0"?"<span class=green>$MSG_Public</span>":"<span class=red>$MSG_Private<span>")."</a></td>";
-        echo "<td><a href=contest_df_change.php?cid=".$row['contest_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey'].">".($row['defunct']=="N"?"<span class=green>$MSG_AVAILABLE </span>":"<span class=red>$MSG_RESERVED </span>")."</a></td>";
+        echo "<td><a href=contest_pr_change.php?cid=".$row['contest_id']."&getkey=".htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8').">".($row['private']=="0"?"<span class=green>$MSG_Public</span>":"<span class=red>$MSG_Private<span>")."</a></td>";
+        echo "<td><a href=contest_df_change.php?cid=".$row['contest_id']."&getkey=".htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8').">".($row['defunct']=="N"?"<span class=green>$MSG_AVAILABLE </span>":"<span class=red>$MSG_RESERVED </span>")."</a></td>";
         echo "<td><a href=contest_edit.php?cid=".$row['contest_id'].">$MSG_EDIT</a></td>";
         echo "<td><a href=contest_add.php?cid=".$row['contest_id'].">$MSG_COPY</a></td>";
         if(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])){
-          echo "<td><a href=\"problem_export_xml.php?cid=".$row['contest_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey']."\">$MSG_EXPORT</a></td>";
+          echo "<td><a href=\"problem_export_xml.php?cid=".$row['contest_id']."&getkey=".htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8')."\">$MSG_EXPORT</a></td>";
         }else{
           echo "<td></td>";
         }
-        echo "<td> <a href=\"../export_contest_code.php?cid=".$row['contest_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey']."\">$MSG_LOG</a></td>";
+        echo "<td> <a href=\"../export_contest_code.php?cid=".$row['contest_id']."&getkey=".htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8')."\">$MSG_LOG</a></td>";
       }else{
         echo "<td colspan=5 align=right><a href=contest_add.php?cid=".$row['contest_id'].">$MSG_COPY</a><td>";
       }

trunk/web/admin/news_list.php
@@ -75,7 +75,7 @@ if(isset($_GET['keyword']) && $_GET['keyword']!=""){
         echo "<td>".$row['news_id']."</td>";
         echo "<td><a href='news_edit.php?id=".$row['news_id']."'>".htmlentities($row['title']==""?"Empty":$row['title'])."</a>"."</td>";
         echo "<td>".$row['time']."</td>";
-        echo "<td><a href=news_df_change.php?id=".$row['news_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey'].">".($row['defunct']=="N"?"<span class=green>On</span>":"<span class=red>Off</span>")."</a>"."</td>";
+        echo "<td><a href=news_df_change.php?id=".$row['news_id']."&getkey=".htmlentities($_SESSION[$OJ_NAME.'_'.'getkey'], ENT_QUOTES, 'UTF-8').">".($row['defunct']=="N"?"<span class=green>On</span>":"<span class=red>Off</span>")."</a>"."</td>";
         echo "<td><a href=news_add_page.php?cid=".$row['news_id'].">Copy</a></td>";
         echo "<td fd='importance'>" . ($row['importance']). "</td>";
         echo "<td fd='menu'>" . ($row['menu'] == 1 ? "YES" : "NO") . "</td>";

trunk/web/admin/phpfm.php
@@ -5,7 +5,7 @@ require_once("../include/my_func.inc.php");
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])
       ||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])
      )){
-	echo $_SESSION[$OJ_NAME.'_'.'administrator'];
+	echo htmlentities($_SESSION[$OJ_NAME.'_'.'administrator'], ENT_QUOTES, 'UTF-8');
 	echo "<a href='../loginpage.php'>Please Login First!</a>";
 	exit(1);
 }

commit 4876201b1fc6e09853d6067c99e0d1f67e035d9d

fix: repair syntax errors in thread.php and newpost.php
Fix byte-level quote bug introduced in 0b60e6d53 - extra quote after
htmlentities() in string concatenation causing parse errors.

Also fix remote_service.php, sidebar/header.php, sidebar/problem1.php

trunk/web/include/remote_service.php
@@ -7,7 +7,7 @@ ini_set("display_errors", "Off");  //set this to "On" for debugging  ,especially
 function is_login($remote_site){
 	$html=curl_get($remote_site);
 	//echo $html;
-	return str_contains($html,"query")); 
+	return str_contains($html,"query"); 
 }
 function show_vcode($remote_site){
 	$url = $remote_site.'/submit'; 

trunk/web/newpost.php
@@ -34,7 +34,7 @@ if (isset($_GET['cid'])) {
             <td width='5%' class='center'></td>
             <td width='15%' class='hidden-xs center'>
                 <?php
-                echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'">".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
+                echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'>'.htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
                 ?>
             </td>
             <td colspan=2>

trunk/web/template/sidebar/header.php
@@ -193,7 +193,7 @@
                             <a class="item" href="portal.php"><i class="tasks icon"></i><?php echo $MSG_TODO;?></a>
                                 <?php if ($OJ_SaaS_ENABLE){ ?>
                                 <?php if($_SERVER['HTTP_HOST']==$DOMAIN)
-                                        echo  "<a class='item' href='http://".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8']).".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
+                                        echo  "<a class='item' href='http://".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
                                 <?php } ?>
                             <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])){ ?>
                             <a class="item" href="admin/"><i class="settings icon"></i><?php echo $MSG_ADMIN;?></a>

trunk/web/template/sidebar/problem1.php
@@ -83,7 +83,7 @@
       echo "<h4>$MSG_Sample_Output</h4><pre class=content><span class=sampledata>".($soutput)."</span></pre><br>";
     }
 
-    echo "<div class=am-g>"
+    echo "<div class=am-g>";
 		echo "<div class=am><strong>$MSG_Sample_Input</strong><pre class=content><span class=sampledata>".($soutput)."</span></pre></div>";
 		echo "<div class=am><strong>$MSG_Sample_Output</strong><pre class=content><span class=sampledata>".($soutput)."</span></pre></div>";
 		echo "</div>"

trunk/web/thread.php
@@ -154,7 +154,7 @@ $isadmin = isset($_SESSION[$OJ_NAME . '_' . 'administrator']);
                 </td>
                 <td width='15%' class='center'>
                     <?php
-                    echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'">".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
+                    echo "<a href='../userinfo.php?user=".htmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8').'>'.hhtmlentities($_SESSION[$OJ_NAME.'_'.'user_id'], ENT_QUOTES, 'UTF-8')."</a>";
                     ?>
                 </td>
                 <td colspan=2>