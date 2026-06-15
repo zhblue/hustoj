2322924b9f3f2612b3b9d2b13d6c76bc055bcc7e
张浩斌 <newsclan@gmail.com>
2026-06-15 20:31:19 +0800

    Add error message for failed post key validation

--- trunk/web/include/check_post_key.php
+++ trunk/web/include/check_post_key.php
@@ -1,5 +1,6 @@
 <?php
 if (!isset($_SESSION[$OJ_NAME.'_'.'postkey'])||!isset($_POST['postkey'])||$_SESSION[$OJ_NAME.'_'.'postkey']!=$_POST['postkey']){
+	echo "post key fail...";
 	exit(1);
 }
 unset($_SESSION[$OJ_NAME.'_'.'postkey']);
