64e0169192db5030f0c2042de2a00bc96cb30883
张浩斌 <newsclan@gmail.com>
2026-06-23 08:30:03 +0800

    Fix delete condition for solution records in bak.sh

--- trunk/install/bak.sh
+++ trunk/install/bak.sh
@@ -46,7 +46,7 @@ delete from source_code_user where solution_id in (select solution_id from solut
 delete from runtimeinfo where solution_id in (select solution_id from solution where problem_id=0 and result>4);
 delete from compileinfo where solution_id in (select solution_id from solution where problem_id=0 and result>4);
 update solution set result=5 where result<4 and in_date<curdate()-interval 3 day;
-delete from solution where problem_id=0 and result>4;
+delete from solution where problem_id<=0 and result>4;
 delete from loginlog where time<curdate()-interval 6 month;
 repair table compileinfo,contest,contest_problem,loginlog,news,privilege,problem,solution,source_code,users,topic,reply,online,sim,mail;
 optimize table compileinfo,contest,contest_problem,loginlog,news,privilege,problem,solution,source_code,users,topic,reply,online,sim,mail;
