f59a10d9592d017ecc19c981cad24c15536e37d4
张浩斌 <newsclan@gmail.com>
2026-08-02 13:59:31 +0800

    Update category.php

--- trunk/web/template/syzoj/category.php
+++ trunk/web/template/syzoj/category.php
@@ -15,8 +15,7 @@
 <script>
         $(document).ready(function(){
                 $('#cates').dblclick(function(){
-                        let cates="系统中有如下的分类，帮我找到它们的相互联系（难度递进或者大类包含小类），用一个html5+js脚本展现，可以用词云或树状的形式显示，每个分类标签用超链
-接指向 形如 /problemset.php?search=关键词 的URL，点击时弹出新窗口。可以使用/template/syzoj/js/echarts.min.js\n "+$(this).text();
+                        let cates="系统中有如下的分类，帮我找到它们的相互联系（难度递进或者大类包含小类），用一个html5+js脚本展现，可以用词云或树状的形式显示，每个分类标签用超链接指向 形如 /problemset.php?search=关键词 的URL，点击时弹出新窗口。可以使用/template/syzoj/js/echarts.min.js\n "+$(this).text();
                         let textToCopy = typeof cates === 'object' ? JSON.stringify(cates) : cates;
 
                         // 创建一个临时的 textarea 元素
