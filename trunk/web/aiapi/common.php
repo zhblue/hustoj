3dfd6478ff2b03db46fd34d4759f3763b6210c5d
张浩斌 <newsclan@gmail.com>
2026-09-25 07:49:14 +0800

    Refactor problem_text variable to use double quotes

--- trunk/web/aiapi/common.php
+++ trunk/web/aiapi/common.php
@@ -121,12 +121,13 @@ if(basename($_SERVER['PHP_SELF'])!=="cron.php"){
 			echo htmlentities($answer[0][0]);
 			exit();
 		}
-		$problem_text= '<br>\n\n## '.$MSG_Description.' <br>\n\n'.$problem['description'];
-		if(!empty($problem['input'])) $problem_text.='<br>\n\n## '.$MSG_Input.'<br>\n\n'.$problem['input'].'<br>\n\n';
-		if(!empty($problem['output'])) $problem_text.='<br>\n\n## '.$MSG_Output.'<br>\n\n'.$problem['output'].'<br>\n\n';
-		if(!empty($problem['sample_input'])) $problem_text.='<br>\n\n## '.$MSG_Sample_Input.'<br>\n\n'.$problem['sample_input'].'<br>\n\n';
-		if(!empty($problem['sample_output'])) $problem_text.='<br>\n\n## '.$MSG_Sample_Output.'<br>\n\n'.$problem['sample_output'].'<br>\n\n';
-		if(!empty($problem['hint'])) $problem_text.='<br>\n\n## '.$MSG_HINT.'<br>\n\n'.$problem['hint'].'<br>\n\n';
+		$problem_text= "<br>\n\n## ".$MSG_Description." <br>\n\n".$problem["description"];
+		if(!empty($problem["input"])) $problem_text.="<br>\n\n## ".$MSG_Input."<br>\n\n".$problem["input"]."<br>\n\n";
+		if(!empty($problem["output"])) $problem_text.="<br>\n\n## ".$MSG_Output."<br>\n\n".$problem["output"]."<br>\n\n";
+		if(!empty($problem["sample_input"])) $problem_text.="<br>\n\n## ".$MSG_Sample_Input."<br>\n\n".$problem["sample_input"]."<br>\n\n";
+		if(!empty($problem["sample_output"])) $problem_text.="<br>\n\n## ".$MSG_Sample_Output."<br>\n\n".$problem["sample_output"]."<br>\n\n";
+		if(!empty($problem["hint"])) $problem_text.="<br>\n\n## ".$MSG_HINT."<br>\n\n".$problem["hint"]."<br>\n\n";
+
 
 		$prompt_user="$MSG_AI_PROMPT_USER_TITLE<br>\n".$problem_text." <br>\n$MSG_AI_PROMPT_USER_SOURCE\n<pre>\n".htmlentities($source)."\n</pre>\n$MSG_AI_PROMPT_USER_ERROR\n<pre>\n".htmlentities($ceinfo)."\n</pre>";
 
